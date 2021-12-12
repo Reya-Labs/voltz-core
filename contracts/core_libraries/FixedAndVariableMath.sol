4a6a32e829cff75c2012128f20b6235e8746dddf
Jamie Rumbelow <jamie@jamierumbelow.net>
2021-12-12 16:35:25 +0000

    contracts: format FixedAndVariableMath

--- contracts/core_libraries/FixedAndVariableMath.sol
+++ contracts/core_libraries/FixedAndVariableMath.sol
@@ -6,220 +6,197 @@ import "prb-math/contracts/PRBMathUD60x18Typed.sol";
 import "./Time.sol";
 
 library FixedAndVariableMath {
-    uint256 public constant SECONDS_IN_YEAR = 31536000 * 10**18;
-    
-    function calculateSettlementCashflow(int256 fixedTokenBalance, int256 variableTokenBalance, uint256 termStartTimestamp, uint256 termEndTimestamp, uint256 variableFactorToMaturity) external view returns(int256 cashflow) {
-        
-        PRBMath.SD59x18 memory fixedCashflow = PRBMathSD59x18Typed.mul(
+  uint256 public constant SECONDS_IN_YEAR = 31536000 * 10**18;
 
-            PRBMath.SD59x18({
-                value: fixedTokenBalance
-            }),
+  function calculateSettlementCashflow(
+    int256 fixedTokenBalance,
+    int256 variableTokenBalance,
+    uint256 termStartTimestamp,
+    uint256 termEndTimestamp,
+    uint256 variableFactorToMaturity
+  ) external view returns (int256 cashflow) {
+    PRBMath.SD59x18 memory fixedCashflow = PRBMathSD59x18Typed.mul(
+      PRBMath.SD59x18({ value: fixedTokenBalance }),
+      PRBMath.SD59x18({
+        value: int256(fixedFactor(true, termStartTimestamp, termEndTimestamp))
+      })
+    );
 
-            PRBMath.SD59x18({
-                value: int256(fixedFactor(true, termStartTimestamp, termEndTimestamp))
-            })
-        );
+    PRBMath.SD59x18 memory variableCashflow = PRBMathSD59x18Typed.mul(
+      PRBMath.SD59x18({ value: variableTokenBalance }),
+      PRBMath.SD59x18({ value: int256(variableFactorToMaturity) })
+    );
 
-        PRBMath.SD59x18 memory variableCashflow = PRBMathSD59x18Typed.mul(
+    cashflow = PRBMathSD59x18Typed.add(fixedCashflow, variableCashflow).value;
+  }
 
-            PRBMath.SD59x18({
-                value: variableTokenBalance
-            }),
+  /// #if_succeeds $result > 0;
+  /// #if_succeeds old(timeInSeconds) > 0;
+  function accrualFact(uint256 timeInSeconds)
+    public
+    pure
+    returns (uint256 timeInYears)
+  {
+    timeInYears = PRBMathUD60x18Typed
+      .div(
+        PRBMath.UD60x18({ value: timeInSeconds }),
+        PRBMath.UD60x18({ value: SECONDS_IN_YEAR })
+      )
+      .value;
+  }
 
-            PRBMath.SD59x18({
-                value: int256(variableFactorToMaturity)
-            })
-        );
+  /// #if_succeeds old(termStartTimestamp) < old(termEndTimestamp);
+  /// #if_succeeds old(atMaturity) == true ==> timeInSeconds == termEndTimestamp - termStartTimestamp;
+  function fixedFactor(
+    bool atMaturity,
+    uint256 termStartTimestamp,
+    uint256 termEndTimestamp
+  ) public view returns (uint256 fixedFactorValue) {
+    // require(
+    //     termEndTimestamp > termStartTimestamp,
+    //     "E<=S"
+    // );
 
-        cashflow = PRBMathSD59x18Typed.add(fixedCashflow, variableCashflow).value;
-    
-    }
-    
-    /// #if_succeeds $result > 0;
-    /// #if_succeeds old(timeInSeconds) > 0;
-    function accrualFact(uint256 timeInSeconds)
-        public
-        pure
-        returns (uint256 timeInYears) {
-        
-        timeInYears = PRBMathUD60x18Typed.div(
-            PRBMath.UD60x18({
-                value: timeInSeconds
-            }),
-            PRBMath.UD60x18({
-                value: SECONDS_IN_YEAR
-            })
-        ).value;
-    }
+    // require(
+    //     Time.blockTimestampScaled()  >= termStartTimestamp,
+    //     "B.T>=S"
+    // );
 
+    // require(
+    //     Time.blockTimestampScaled()  <= termEndTimestamp,
+    //     "B.T>=S"
+    // );
 
-    /// #if_succeeds old(termStartTimestamp) < old(termEndTimestamp);
-    /// #if_succeeds old(atMaturity) == true ==> timeInSeconds == termEndTimestamp - termStartTimestamp;
-    function fixedFactor(
-        bool atMaturity,
-        uint256 termStartTimestamp,
-        uint256 termEndTimestamp
-    ) public view returns (uint256 fixedFactorValue) {
+    uint256 timeInSeconds;
 
-        // require(
-        //     termEndTimestamp > termStartTimestamp,
-        //     "E<=S"
-        // );
-
-        // require(
-        //     Time.blockTimestampScaled()  >= termStartTimestamp,
-        //     "B.T>=S"
-        // );
-
-        // require(
-        //     Time.blockTimestampScaled()  <= termEndTimestamp,
-        //     "B.T>=S"
-        // );
-
-        uint256 timeInSeconds;
-
-        if (atMaturity) {
-            timeInSeconds = PRBMathUD60x18Typed.sub(
-                PRBMath.UD60x18({
-                    value: termEndTimestamp
-                }),
-                PRBMath.UD60x18({
-                    value: termStartTimestamp
-                })
-            ).value;
-        } else {
-            timeInSeconds = Time.blockTimestampScaled() - termStartTimestamp;
-        }
-
-        uint256 timeInYears = accrualFact(timeInSeconds);
-
-        fixedFactorValue = PRBMathUD60x18Typed
-            .mul(
-                PRBMath.UD60x18({value: timeInYears}),
-                PRBMath.UD60x18({value: 10**16})
-            )
-            .value;
+    if (atMaturity) {
+      timeInSeconds = PRBMathUD60x18Typed
+        .sub(
+          PRBMath.UD60x18({ value: termEndTimestamp }),
+          PRBMath.UD60x18({ value: termStartTimestamp })
+        )
+        .value;
+    } else {
+      timeInSeconds = Time.blockTimestampScaled() - termStartTimestamp;
     }
 
+    uint256 timeInYears = accrualFact(timeInSeconds);
 
-    /// #if_succeeds old(termStartTimestamp) < old(termEndTimestamp);
-    /// #if_succeeds excessBalance < 0 ==> fixedTokenBalance > amount0;
-    /// #if_succeeds excessBalance > 0 ==> fixedTokenBalance < amount0;
-    function calculateFixedTokenBalance(
-        int256 amount0,
-        int256 excessBalance,
-        uint256 termStartTimestamp,
-        uint256 termEndTimestamp
-    ) internal view returns (int256 fixedTokenBalance) {
+    fixedFactorValue = PRBMathUD60x18Typed
+      .mul(
+        PRBMath.UD60x18({ value: timeInYears }),
+        PRBMath.UD60x18({ value: 10**16 })
+      )
+      .value;
+  }
 
-        require(
-            termEndTimestamp > termStartTimestamp,
-            "E<=S"
-        );
-     
-        // expected fixed cashflow with unbalanced number of fixed tokens
-        PRBMath.SD59x18 memory exp1 = PRBMathSD59x18Typed.mul(
-            PRBMath.SD59x18({value: amount0}),
-            PRBMath.SD59x18({
-                value: int256(
-                    fixedFactor(true, termStartTimestamp, termEndTimestamp)
-                )
-            })
-        );
+  /// #if_succeeds old(termStartTimestamp) < old(termEndTimestamp);
+  /// #if_succeeds excessBalance < 0 ==> fixedTokenBalance > amount0;
+  /// #if_succeeds excessBalance > 0 ==> fixedTokenBalance < amount0;
+  function calculateFixedTokenBalance(
+    int256 amount0,
+    int256 excessBalance,
+    uint256 termStartTimestamp,
+    uint256 termEndTimestamp
+  ) internal view returns (int256 fixedTokenBalance) {
+    require(termEndTimestamp > termStartTimestamp, "E<=S");
 
-        // fixed cashflow  with balanced number of fixed tokens
-        PRBMath.SD59x18 memory numerator = PRBMathSD59x18Typed.sub(
-            exp1,
-            PRBMath.SD59x18({value: excessBalance})
-        );
+    // expected fixed cashflow with unbalanced number of fixed tokens
+    PRBMath.SD59x18 memory exp1 = PRBMathSD59x18Typed.mul(
+      PRBMath.SD59x18({ value: amount0 }),
+      PRBMath.SD59x18({
+        value: int256(fixedFactor(true, termStartTimestamp, termEndTimestamp))
+      })
+    );
 
+    // fixed cashflow  with balanced number of fixed tokens
+    PRBMath.SD59x18 memory numerator = PRBMathSD59x18Typed.sub(
+      exp1,
+      PRBMath.SD59x18({ value: excessBalance })
+    );
 
-        // fixed token balance that takes into account acrrued cashflows
-        fixedTokenBalance = PRBMathSD59x18Typed
-            .div(
-                numerator,
-                PRBMath.SD59x18({
-                    value: int256(
-                        fixedFactor(true, termStartTimestamp, termEndTimestamp)
-                    )
-                })
-            )
-            .value;
-    }
+    // fixed token balance that takes into account acrrued cashflows
+    fixedTokenBalance = PRBMathSD59x18Typed
+      .div(
+        numerator,
+        PRBMath.SD59x18({
+          value: int256(fixedFactor(true, termStartTimestamp, termEndTimestamp))
+        })
+      )
+      .value;
+  }
 
-    
-    struct AccruedValues {
-        int256 excessFixedAccruedBalance;
-        int256 excessVariableAccruedBalance;
-        int256 excessBalance;
-    }
-    
+  struct AccruedValues {
+    int256 excessFixedAccruedBalance;
+    int256 excessVariableAccruedBalance;
+    int256 excessBalance;
+  }
 
+  function getExcessBalance(
+    int256 amount0,
+    int256 amount1,
+    uint256 accruedVariableFactor,
+    uint256 termStartTimestamp,
+    uint256 termEndTimestamp
+  ) internal view returns (int256) {
+    AccruedValues memory accruedValues;
 
-    function getExcessBalance(
-        int256 amount0,
-        int256 amount1,
-        uint256 accruedVariableFactor,
-        uint256 termStartTimestamp,
-        uint256 termEndTimestamp) internal view returns (int256) {
+    accruedValues.excessFixedAccruedBalance = PRBMathSD59x18Typed
+      .mul(
+        PRBMath.SD59x18({ value: amount0 }),
+        PRBMath.SD59x18({
+          value: int256(
+            fixedFactor(false, termStartTimestamp, termEndTimestamp)
+          )
+        })
+      )
+      .value;
 
-        AccruedValues memory accruedValues;
+    accruedValues.excessVariableAccruedBalance = PRBMathSD59x18Typed
+      .mul(
+        PRBMath.SD59x18({ value: amount1 }),
+        PRBMath.SD59x18({ value: int256(accruedVariableFactor) })
+      )
+      .value;
 
-        accruedValues.excessFixedAccruedBalance = PRBMathSD59x18Typed.mul(
-                PRBMath.SD59x18({value: amount0}),
-                PRBMath.SD59x18({
-                    value: int256(
-                        fixedFactor(
-                            false,
-                            termStartTimestamp,
-                            termEndTimestamp
-                        )
-                    )
-                })
-        ).value;
+    accruedValues.excessBalance = PRBMathSD59x18Typed
+      .add(
+        PRBMath.SD59x18({ value: accruedValues.excessFixedAccruedBalance }),
+        PRBMath.SD59x18({ value: accruedValues.excessVariableAccruedBalance })
+      )
+      .value;
 
-        accruedValues.excessVariableAccruedBalance = PRBMathSD59x18Typed.mul(
-                PRBMath.SD59x18({value: amount1}),
-                PRBMath.SD59x18({value: int256(accruedVariableFactor)})
-        ).value;
+    return accruedValues.excessBalance;
+  }
 
-        accruedValues.excessBalance = PRBMathSD59x18Typed.add(
-                PRBMath.SD59x18({value: accruedValues.excessFixedAccruedBalance}),
-                PRBMath.SD59x18({value: accruedValues.excessVariableAccruedBalance})
-        ).value;
+  /// #if_succeeds termEndTimestamp > termStartTimestamp;
+  function getFixedTokenBalance(
+    int256 amount0,
+    int256 amount1,
+    uint256 accruedVariableFactor,
+    uint256 termStartTimestamp,
+    uint256 termEndTimestamp
+  ) public view returns (int256 fixedTokenBalance) {
+    // todo: check that amount0 and amount1 are of different signs? (scribble?)
 
-        return accruedValues.excessBalance;
+    // require(
+    //     termEndTimestamp > termStartTimestamp,
+    //     "E<=S"
+    // );
 
-    }
-    
-    
+    int256 excessBalance = getExcessBalance(
+      amount0,
+      amount1,
+      accruedVariableFactor,
+      termStartTimestamp,
+      termEndTimestamp
+    );
 
-    /// #if_succeeds termEndTimestamp > termStartTimestamp;
-    function getFixedTokenBalance(
-        int256 amount0,
-        int256 amount1,
-        uint256 accruedVariableFactor,
-        uint256 termStartTimestamp,
-        uint256 termEndTimestamp
-    ) public view returns (int256 fixedTokenBalance) {
-
-        // todo: check that amount0 and amount1 are of different signs? (scribble?)
-        
-        // require(
-        //     termEndTimestamp > termStartTimestamp,
-        //     "E<=S"
-        // );
-
-        int256 excessBalance = getExcessBalance(amount0, amount1, accruedVariableFactor, termStartTimestamp, termEndTimestamp);
-        
-        fixedTokenBalance = calculateFixedTokenBalance(
-            amount0,
-            excessBalance,
-            termStartTimestamp,
-            termEndTimestamp
-        );
-
-    }
-    
+    fixedTokenBalance = calculateFixedTokenBalance(
+      amount0,
+      excessBalance,
+      termStartTimestamp,
+      termEndTimestamp
+    );
+  }
 }
