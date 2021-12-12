d594fe9d24bfd44b920aa9c5b81f9e01dbc57071
Jamie Rumbelow <jamie@jamierumbelow.net>
2021-12-12 17:00:42 +0000

    contracts: document FixedAndVariableMath
    also adds a couple of requirements

--- contracts/core_libraries/FixedAndVariableMath.sol
+++ contracts/core_libraries/FixedAndVariableMath.sol
@@ -5,9 +5,20 @@ import "prb-math/contracts/PRBMathSD59x18Typed.sol";
 import "prb-math/contracts/PRBMathUD60x18Typed.sol";
 import "./Time.sol";
 
+/// @title A utility library for mathematics of fixed and variable token amounts.
+/// @author Artur Begyan
 library FixedAndVariableMath {
+  /// @notice Number of wei-seconds in a year
+  /// @dev Ignoring leap years since we're only using it to calculate the eventual APY rate
   uint256 public constant SECONDS_IN_YEAR = 31536000 * 10**18;
 
+  /// @notice Caclulate the remaining cashflow to settle a position
+  /// @param fixedTokenBalance The current balance of the fixed side of the position
+  /// @param variableTokenBalance The current balance of the variable side of the position
+  /// @param termStartTimestamp When did the position begin, in seconds
+  /// @param termEndTimestamp When does the position reach maturity, in seconds
+  /// @param variableFactorToMaturity What factor expresses the current remaining variable rate, up to position maturity?
+  /// @return cashflow The remaining cashflow of the position
   function calculateSettlementCashflow(
     int256 fixedTokenBalance,
     int256 variableTokenBalance,
@@ -30,6 +41,10 @@ library FixedAndVariableMath {
     cashflow = PRBMathSD59x18Typed.add(fixedCashflow, variableCashflow).value;
   }
 
+  /// @notice Divide a given time in seconds by the number of wei-seconds in a year
+  /// @param timeInSeconds A time in wei-seconds
+  /// @return timeInYears An annualised factor of timeInSeconds
+  ///
   /// #if_succeeds $result > 0;
   /// #if_succeeds old(timeInSeconds) > 0;
   function accrualFact(uint256 timeInSeconds)
@@ -45,6 +60,12 @@ library FixedAndVariableMath {
       .value;
   }
 
+  /// @notice Calculate the fixed factor for a position
+  /// @param atMaturity Whether to calculate the factor at maturity (true), or now (false)
+  /// @param termStartTimestamp When does the period of time begin, in wei-seconds
+  /// @param termEndTimestamp When does the period of time end, in wei-seconds
+  /// @return fixedFactorValue The fixed factor for the position
+  /// 
   /// #if_succeeds old(termStartTimestamp) < old(termEndTimestamp);
   /// #if_succeeds old(atMaturity) == true ==> timeInSeconds == termEndTimestamp - termStartTimestamp;
   function fixedFactor(
@@ -52,20 +73,20 @@ library FixedAndVariableMath {
     uint256 termStartTimestamp,
     uint256 termEndTimestamp
   ) public view returns (uint256 fixedFactorValue) {
-    // require(
-    //     termEndTimestamp > termStartTimestamp,
-    //     "E<=S"
-    // );
+    require(
+        termEndTimestamp > termStartTimestamp,
+        "E<=S"
+    );
 
-    // require(
-    //     Time.blockTimestampScaled()  >= termStartTimestamp,
-    //     "B.T>=S"
-    // );
+    require(
+        Time.blockTimestampScaled() >= termStartTimestamp,
+        "B.T>=S"
+    );
 
-    // require(
-    //     Time.blockTimestampScaled()  <= termEndTimestamp,
-    //     "B.T>=S"
-    // );
+    require(
+        Time.blockTimestampScaled() <= termEndTimestamp,
+        "B.T>=S"
+    );
 
     uint256 timeInSeconds;
 
@@ -90,6 +111,13 @@ library FixedAndVariableMath {
       .value;
   }
 
+  /// @notice Calculate the fixed token balance for a position over a timespan
+  /// @param amount0 A fixed amount
+  /// @param excessBalance Any excess balance from the variable side of the position
+  /// @param termStartTimestamp When does the period of time begin, in wei-seconds
+  /// @param termEndTimestamp When does the period of time end, in wei-seconds
+  /// @return fixedTokenBalance The fixed token balance for that time period
+  ///  
   /// #if_succeeds old(termStartTimestamp) < old(termEndTimestamp);
   /// #if_succeeds excessBalance < 0 ==> fixedTokenBalance > amount0;
   /// #if_succeeds excessBalance > 0 ==> fixedTokenBalance < amount0;
@@ -126,12 +154,20 @@ library FixedAndVariableMath {
       .value;
   }
 
+  /// @notice Represent excess values accrued in some period
   struct AccruedValues {
     int256 excessFixedAccruedBalance;
     int256 excessVariableAccruedBalance;
     int256 excessBalance;
   }
 
+  /// @notice Calculate the excess balance of both sides of a position
+  /// @param amount0 A fixed balance
+  /// @param amount1 A variable balance
+  /// @param accruedVariableFactor An annualised factor in wei-years
+  /// @param termStartTimestamp When does the period of time begin, in wei-seconds
+  /// @param termEndTimestamp When does the period of time end, in wei-seconds
+  /// @return excessBalance The excess balance
   function getExcessBalance(
     int256 amount0,
     int256 amount1,
@@ -169,6 +205,14 @@ library FixedAndVariableMath {
     return accruedValues.excessBalance;
   }
 
+  /// @notice Calculate the fixed token balance given both fixed and variable balances
+  /// @param amount0 A fixed balance
+  /// @param amount1 A variable balance
+  /// @param accruedVariableFactor An annualised factor in wei-years
+  /// @param termStartTimestamp When does the period of time begin, in wei-seconds
+  /// @param termEndTimestamp When does the period of time end, in wei-seconds
+  /// @return fixedTokenBalance The fixed token balance for that time period
+  ///
   /// #if_succeeds termEndTimestamp > termStartTimestamp;
   function getFixedTokenBalance(
     int256 amount0,
@@ -177,12 +221,15 @@ library FixedAndVariableMath {
     uint256 termStartTimestamp,
     uint256 termEndTimestamp
   ) public view returns (int256 fixedTokenBalance) {
-    // todo: check that amount0 and amount1 are of different signs? (scribble?)
+    require(
+      amount0 ^ amount1 < 0,
+      "amount0 and amount1 must have different signs"
+    );
 
-    // require(
-    //     termEndTimestamp > termStartTimestamp,
-    //     "E<=S"
-    // );
+    require(
+        termEndTimestamp > termStartTimestamp,
+        "E<=S"
+    );
 
     int256 excessBalance = getExcessBalance(
       amount0,
