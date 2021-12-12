87fdb62e4146902c092de925434fa1eb0190dbd6
Jamie Rumbelow <jamie@jamierumbelow.net>
2021-12-12 17:21:09 +0000

    contracts: document UnwindTraderUnwindPosition

--- contracts/core_libraries/UnwindTraderUnwindPosition.sol
+++ contracts/core_libraries/UnwindTraderUnwindPosition.sol
@@ -8,104 +8,111 @@ import "../utils/TickMath.sol";
 import "../core_libraries/Position.sol";
 import "../core_libraries/Tick.sol";
 
+/// @title Utilities for unwinding traders (fixed / variable takers) and positions (LPs)
+/// @author Artur Begyan
 library UnwindTraderUnwindPosition {
-    using Position for mapping(bytes32 => Position.Info);
-    using Position for Position.Info;
+  using Position for mapping(bytes32 => Position.Info);
+  using Position for Position.Info;
 
+  /// @notice Unwind a trader in a given market
+  /// @param ammAddress The address of the IAMM contract to unwind (note: this is different from the corresponding vAMM)
+  /// @param traderAddress The address of the trader to unwind
+  /// @param notional The number of tokens to unwind (the opposite of the trade, so positive – variable tokens – for fixed takers, and negative – fixed tokens - for variable takers, such that fixed tokens + variable tokens = 0)
+  function unwindTrader(
+    address ammAddress,
+    address traderAddress,
+    int256 notional
+  ) external {
+    IAMM amm = IAMM(ammAddress);
 
-    function unwindTrader(
-        address ammAddress, // not: different from the vAMM
-        address traderAddress,
-        int256 notional
-    ) external {
-        
-        IAMM amm = IAMM(ammAddress);
+    bool isFT = notional > 0;
 
-        bool isFT = notional > 0;
+    if (isFT) {
+      // get into a VT swap
+      // notional is positive
+      IVAMM.SwapParams memory params = IVAMM.SwapParams({
+        recipient: traderAddress,
+        isFT: !isFT,
+        amountSpecified: -notional,
+        sqrtPriceLimitX96: TickMath.MIN_SQRT_RATIO,
+        // todo: the last three need to be double checked
+        isUnwind: true,
+        isTrader: true
+      });
 
-        if (isFT) {
-            // get into a VT swap
-            // notional is positive
-            IVAMM.SwapParams memory params = IVAMM.SwapParams({
-               recipient: traderAddress,
-               isFT: !isFT,
-               amountSpecified: -notional,
-               sqrtPriceLimitX96: TickMath.MIN_SQRT_RATIO,
-               // todo: the last three need to be double checked
-               isUnwind: true,
-               isTrader: true
-            });
+      amm.swap(params);
+    } else {
+      // get into an FT swap
+      // notional is negative
 
-            amm.swap(params);
+      IVAMM.SwapParams memory params = IVAMM.SwapParams({
+        recipient: traderAddress,
+        isFT: isFT,
+        amountSpecified: notional,
+        sqrtPriceLimitX96: TickMath.MAX_SQRT_RATIO,
+        isUnwind: true,
+        isTrader: true
+      });
 
-        } else {
-            // get into an FT swap
-            // notional is negative
-            
-            IVAMM.SwapParams memory params = IVAMM.SwapParams({
-               recipient: traderAddress,
-               isFT: isFT,
-               amountSpecified: notional,
-               sqrtPriceLimitX96: TickMath.MAX_SQRT_RATIO,
-               isUnwind: true,
-               isTrader: true
-            });
-
-            amm.swap(params);
-
-        }
+      amm.swap(params);
     }
+  }
 
+  /// @notice Unwind an LP position for a given tick range
+  /// @param ammAddress The address of the IAMM contract to unwind (note: this is different from the corresponding vAMM)
+  /// @param owner The address of the LP to unwind
+  /// @param tickLower The lower bound of the tick range, inclusive
+  /// @param tickUpper The upper bound of the tick range, inclusive
+  /// @param position The corresponding Position.Info struct that represents the position
+  /// @return _fixedTokenBalance The remaining fixed token balance
+  /// @return _variableTokenBalance The remaining variable token balance
+  function unwindPosition(
+    address ammAddress,
+    address owner,
+    int24 tickLower,
+    int24 tickUpper,
+    Position.Info memory position
+  ) external returns (int256 _fixedTokenBalance, int256 _variableTokenBalance) {
+    IAMM amm = IAMM(ammAddress);
+    Tick.checkTicks(tickLower, tickUpper);
 
-    function unwindPosition(
-        address ammAddress,
-        address owner,
-        int24 tickLower,
-        int24 tickUpper,
-        Position.Info memory position
-    ) external returns(int256 _fixedTokenBalance, int256 _variableTokenBalance) {
+    require(
+      position.variableTokenBalance != 0,
+      "no need to unwind a net zero position"
+    );
 
-        IAMM amm = IAMM(ammAddress);
+    // todo: before checking the variable or fixed token balances, we need to make sure they have been updated (check lastTimestamp)
+    // initiate a swap
+    bool isFT = position.fixedTokenBalance > 0;
 
-        Tick.checkTicks(tickLower, tickUpper); 
-        require(position.variableTokenBalance!=0, "no need to unwind a net zero position");
-        
-        // todo: before checking the variable or fixed token balances, we need to make sure they have been updated (check lastTimestamp)
-        // initiate a swap
-        bool isFT = position.fixedTokenBalance > 0;
+    if (isFT) {
+      // get into a VT swap
+      // variableTokenBalance is negative
 
-        if (isFT) {
-            // get into a VT swap
-            // variableTokenBalance is negative
+      IVAMM.SwapParams memory params = IVAMM.SwapParams({
+        recipient: owner,
+        isFT: !isFT,
+        amountSpecified: position.variableTokenBalance, // todo: double check the sign
+        sqrtPriceLimitX96: TickMath.MIN_SQRT_RATIO,
+        isUnwind: true,
+        isTrader: false
+      });
 
-            IVAMM.SwapParams memory params = IVAMM.SwapParams({
-               recipient: owner,
-               isFT: !isFT,
-               amountSpecified: position.variableTokenBalance, // todo: double check the sign 
-               sqrtPriceLimitX96: TickMath.MIN_SQRT_RATIO,
-               isUnwind: true,
-               isTrader: false
-            });
+      (_fixedTokenBalance, _variableTokenBalance) = amm.swap(params); // todo: check the outputs are correct
+    } else {
+      // get into an FT swap
+      // variableTokenBalance is positive
 
-            (_fixedTokenBalance, _variableTokenBalance) = amm.swap(params); // todo: check the outputs are correct
+      IVAMM.SwapParams memory params = IVAMM.SwapParams({
+        recipient: owner,
+        isFT: isFT,
+        amountSpecified: position.variableTokenBalance,
+        sqrtPriceLimitX96: TickMath.MAX_SQRT_RATIO,
+        isUnwind: true,
+        isTrader: false
+      });
 
-        } else {
-            // get into an FT swap
-            // variableTokenBalance is positive
-            
-            IVAMM.SwapParams memory params = IVAMM.SwapParams({
-               recipient: owner,
-               isFT: isFT,
-               amountSpecified: position.variableTokenBalance,
-               sqrtPriceLimitX96: TickMath.MAX_SQRT_RATIO,
-               isUnwind: true,
-               isTrader: false
-            });
-
-            (_fixedTokenBalance, _variableTokenBalance) = amm.swap(params); 
-
-        }    
-
+      (_fixedTokenBalance, _variableTokenBalance) = amm.swap(params);
     }
-
+  }
 }
