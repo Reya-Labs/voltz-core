a3e8d8e64bf5d103ce4f0735a460a8123576221a
Jamie Rumbelow <jamie@jamierumbelow.net>
2021-12-12 17:31:09 +0000

    contracts: format AMM

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -27,180 +27,212 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "./interfaces/IMarginEngine.sol";
 import "./interfaces/IVAMM.sol";
 
-
 contract AMM is IAMM, Pausable {
+  using LowGasSafeMath for uint256;
+  using LowGasSafeMath for int256;
 
-    using LowGasSafeMath for uint256;
-    using LowGasSafeMath for int256;
-    
-    using Position for mapping(bytes32 => Position.Info);
-    using Position for Position.Info;
+  using Position for mapping(bytes32 => Position.Info);
+  using Position for Position.Info;
 
-    using Trader for mapping(bytes32 => Trader.Info);
-    using Trader for Trader.Info;
+  using Trader for mapping(bytes32 => Trader.Info);
+  using Trader for Trader.Info;
 
-    address public override factory;
+  address public override factory;
 
-    address public immutable override underlyingToken;
+  address public immutable override underlyingToken;
 
-    bytes32 public override rateOracleId;
+  bytes32 public override rateOracleId;
 
-    uint256 public immutable override termStartTimestamp;
+  uint256 public immutable override termStartTimestamp;
 
-    uint256 public immutable override termEndTimestamp;
+  uint256 public immutable override termEndTimestamp;
 
-    IMarginCalculator public override calculator;
-    
-    IRateOracle public override rateOracle;
+  IMarginCalculator public override calculator;
 
-    IVAMM public override vamm;  
-    IMarginEngine public override marginEngine;
-    bool public override unlocked;
+  IRateOracle public override rateOracle;
 
-    constructor() Pausable() {
-        (
-            factory,
-            underlyingToken,
-            rateOracleId,
-            termStartTimestamp,
-            termEndTimestamp
-        ) = IDeployer(msg.sender).ammParameters();
-        
-        address rateOracleAddress = IFactory(factory).getRateOracleAddress(rateOracleId);
+  IVAMM public override vamm;
+  IMarginEngine public override marginEngine;
+  bool public override unlocked;
 
-        rateOracle = IRateOracle(rateOracleAddress);
+  constructor() Pausable() {
+    (
+      factory,
+      underlyingToken,
+      rateOracleId,
+      termStartTimestamp,
+      termEndTimestamp
+    ) = IDeployer(msg.sender).ammParameters();
 
-        address calculatorAddress = IFactory(factory).calculator();
-        
-        calculator = IMarginCalculator(calculatorAddress);
+    address rateOracleAddress = IFactory(factory).getRateOracleAddress(
+      rateOracleId
+    );
+
+    rateOracle = IRateOracle(rateOracleAddress);
+
+    address calculatorAddress = IFactory(factory).calculator();
+
+    calculator = IMarginCalculator(calculatorAddress);
+  }
+
+  modifier onlyFactoryOwner() {
+    require(IFactory(factory).owner() != address(0));
+    require(msg.sender == IFactory(factory).owner());
+    _;
+  }
+
+  modifier onlyVAMM() {
+    require(address(vamm) != address(0));
+    require(msg.sender == address(vamm));
+    _;
+  }
+
+  /// @dev Mutually exclusive reentrancy protection into the vamm to/from a method. This method also prevents entrance
+  /// to a function before the pool is initialized. The reentrancy guard is required throughout the contract because
+  /// we use balance checks to determine the payment status of interactions such as mint, swap and flash.
+  modifier lock() {
+    require(unlocked, "LOK");
+    unlocked = false;
+    _;
+    unlocked = true;
+  }
+
+  function getSlot0() external view override returns (IVAMM.Slot0 memory) {
+    (uint160 sqrtPriceX96, int24 tick, uint256 feeProtocol) = vamm.slot0();
+
+    return
+      IVAMM.Slot0({
+        sqrtPriceX96: sqrtPriceX96,
+        tick: tick,
+        feeProtocol: feeProtocol
+      });
+  }
+
+  function getVariableTokenGrowthGlobal()
+    external
+    view
+    override
+    returns (int256)
+  {
+    return vamm.variableTokenGrowthGlobal();
+  }
+
+  function getFixedTokenGrowthGlobal() external view override returns (int256) {
+    return vamm.fixedTokenGrowthGlobal();
+  }
+
+  function setVAMM(address _vAMMAddress) external override onlyFactoryOwner {
+    vamm = IVAMM(_vAMMAddress);
+  }
+
+  function setMarginEngine(address _marginEngine)
+    external
+    override
+    onlyFactoryOwner
+  {
+    marginEngine = IMarginEngine(_marginEngine);
+  }
+
+  function setUnlocked(bool _unlocked) external override onlyVAMM {
+    unlocked = _unlocked;
+  }
+
+  function updatePositionMargin(
+    IMarginEngine.ModifyPositionParams memory params,
+    int256 marginDelta
+  ) external override {
+    marginEngine.updatePositionMargin(params, marginDelta);
+  }
+
+  function updateTraderMargin(address recipient, int256 marginDelta)
+    external
+    override
+  {
+    marginEngine.updateTraderMargin(recipient, marginDelta);
+  }
+
+  function settlePosition(IMarginEngine.ModifyPositionParams memory params)
+    external
+    override
+  {
+    marginEngine.settlePosition(params);
+  }
+
+  function settleTrader(address recipient) external override {
+    marginEngine.settleTrader(recipient);
+  }
+
+  function liquidatePosition(IMarginEngine.ModifyPositionParams memory params)
+    external
+    override
+    whenNotPaused
+  {
+    marginEngine.liquidatePosition(params);
+  }
+
+  function liquidateTrader(address traderAddress)
+    external
+    override
+    whenNotPaused
+  {
+    marginEngine.liquidateTrader(traderAddress);
+  }
+
+  function burn(
+    int24 tickLower,
+    int24 tickUpper,
+    uint128 amount
+  ) external override lock {
+    vamm.burn(tickLower, tickUpper, amount);
+  }
+
+  function mint(
+    address recipient,
+    int24 tickLower,
+    int24 tickUpper,
+    uint128 amount
+  ) external override whenNotPaused lock {
+    vamm.mint(recipient, tickLower, tickUpper, amount); // todo: how costly are these calls?
+  }
+
+  function swap(IVAMM.SwapParams memory params)
+    external
+    override
+    whenNotPaused
+    lock
+    returns (int256 _fixedTokenDelta, int256 _variableTokenDelta)
+  {
+    (_fixedTokenDelta, _variableTokenDelta) = vamm.swap(params);
+  }
+
+  function setFeeProtocol(uint256 feeProtocol)
+    external
+    override
+    lock
+    onlyFactoryOwner
+  {
+    vamm.setFeeProtocol(feeProtocol);
+  }
+
+  function collectProtocol(address recipient, uint256 amountRequested)
+    external
+    override
+    lock
+    onlyFactoryOwner
+    returns (uint256 amount)
+  {
+    // moved this logic to vamm.updateProtocolFees
+    // amount = amountRequested > protocolFees ? protocolFees : amountRequested;
+
+    if (amount > 0) {
+      vamm.updateProtocolFees(amount);
+      IERC20Minimal(underlyingToken).transferFrom(
+        address(this),
+        recipient,
+        amount
+      );
     }
-    
-    modifier onlyFactoryOwner() {
-        require(IFactory(factory).owner() != address(0));
-        require(msg.sender == IFactory(factory).owner());
-        _;
-    }
 
-    modifier onlyVAMM() {
-        require(address(vamm) != address(0));
-        require(msg.sender == address(vamm));
-        _;
-    }
-
-    
-    /// @dev Mutually exclusive reentrancy protection into the vamm to/from a method. This method also prevents entrance
-    /// to a function before the pool is initialized. The reentrancy guard is required throughout the contract because
-    /// we use balance checks to determine the payment status of interactions such as mint, swap and flash.
-    modifier lock() {
-        require(unlocked, "LOK");
-        unlocked = false;
-        _;
-        unlocked = true;
-    }
-
-    function getSlot0() external view override returns (IVAMM.Slot0 memory) {
-
-        (uint160 sqrtPriceX96, int24 tick, uint256 feeProtocol) = vamm.slot0();
-
-        return IVAMM.Slot0(
-            {
-                sqrtPriceX96: sqrtPriceX96,
-                tick: tick,
-                feeProtocol: feeProtocol
-            }
-        );
-    }
-
-    function getVariableTokenGrowthGlobal() external view override returns (int256) {
-        return vamm.variableTokenGrowthGlobal();
-    }
-
-    function getFixedTokenGrowthGlobal() external view override returns (int256) {
-        return vamm.fixedTokenGrowthGlobal();
-    }
-    
-    function setVAMM(address _vAMMAddress) external onlyFactoryOwner override {
-        vamm = IVAMM(_vAMMAddress);
-    }
-
-    function setMarginEngine(address _marginEngine) external onlyFactoryOwner override {
-        marginEngine = IMarginEngine(_marginEngine);
-    }
-
-    function setUnlocked(bool _unlocked) external onlyVAMM override {
-        unlocked = _unlocked;
-    }
-
-    function updatePositionMargin(IMarginEngine.ModifyPositionParams memory params, int256 marginDelta) external override {
-        marginEngine.updatePositionMargin(params, marginDelta);
-    }
-
-    function updateTraderMargin(address recipient, int256 marginDelta) external override {
-        marginEngine.updateTraderMargin(recipient, marginDelta);
-    }
-    
-    function settlePosition(IMarginEngine.ModifyPositionParams memory params) external override {
-        marginEngine.settlePosition(params);
-    }
-    
-    function settleTrader(address recipient) external override  {
-        marginEngine.settleTrader(recipient);
-    }
-    
-    function liquidatePosition(IMarginEngine.ModifyPositionParams memory params) whenNotPaused external override {
-        marginEngine.liquidatePosition(params);
-    }
-
-    function liquidateTrader(address traderAddress) whenNotPaused external override {
-        marginEngine.liquidateTrader(traderAddress);
-    }
-    
-
-    
-    function burn(
-        int24 tickLower,
-        int24 tickUpper,
-        uint128 amount
-    ) external override lock {
-        vamm.burn(tickLower, tickUpper, amount);
-    }
-    
-
-    function mint(
-        address recipient,
-        int24 tickLower,
-        int24 tickUpper,
-        uint128 amount
-    ) external override whenNotPaused lock {
-        vamm.mint(recipient, tickLower, tickUpper, amount); // todo: how costly are these calls?
-    }
-    
-    function swap(
-        IVAMM.SwapParams memory params
-    ) external override whenNotPaused lock returns (int256 _fixedTokenDelta, int256 _variableTokenDelta) {
-        
-        (_fixedTokenDelta, _variableTokenDelta) = vamm.swap(params);
-
-    }
-
-    function setFeeProtocol(uint256 feeProtocol) external lock override onlyFactoryOwner  {
-        vamm.setFeeProtocol(feeProtocol);
-    }
-
-    function collectProtocol(
-        address recipient,
-        uint256 amountRequested
-    ) external lock override onlyFactoryOwner returns (uint256 amount){
-
-        // moved this logic to vamm.updateProtocolFees
-        // amount = amountRequested > protocolFees ? protocolFees : amountRequested;
-
-        if (amount > 0) {
-            vamm.updateProtocolFees(amount);
-            IERC20Minimal(underlyingToken).transferFrom(address(this), recipient, amount);
-        }
-
-        // todo: emit collect protocol event
-    }
-
+    // todo: emit collect protocol event
+  }
 }
