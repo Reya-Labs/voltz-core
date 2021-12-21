020915066e7bab86f63b701d11de9aea7f85c621
arturbegyan <arturbegyan98@gmail.com>
2021-12-21 23:47:44 +0300

    callee now has an abi

--- contracts/test/TestMarginEngineCallee.sol
+++ contracts/test/TestMarginEngineCallee.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.8.0;
 import "../MarginEngine.sol";
+import "../interfaces/IMarginEngine.sol";
 
 
 contract TestMarginEngineCallee {
   
+  function updateTraderMarginTest(address marginEngine, address recipient, int256 marginDelta) external {
+    IMarginEngine(marginEngine).updateTraderMargin(recipient, marginDelta);
+  }
+
 }
