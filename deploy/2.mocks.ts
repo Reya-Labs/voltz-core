386ff745490cce2c8dd315b8231f41a5d3a2d920
CyclopsRex <cyclopsrex@voltz.xyz>
2022-01-31 16:52:59 +0000

    Initial hardhat-deploy scripts.
    - Add some `hardhat-deploy` scripts to aid deployment and to track the resulting deployment data
    - Add npm command for running a local testnet (uses `hardhat node`)

---
+++ deploy/2.mocks.ts
@@ -0,0 +1,28 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+import { BigNumber } from "ethers";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+  const doLogging = true;
+
+  // Mock contracts that should/need not be deployed on mainnet
+  const mockERC20Deploy = await deploy("ERC20Mock", {
+    from: deployer,
+    args: ["Voltz USD", "VUSD", 6],
+    log: doLogging,
+  });
+  await deploy("MockAaveLendingPool", {
+    from: deployer,
+    log: doLogging,
+  });
+  const mockAaveLendingPool = await ethers.getContract("MockAaveLendingPool");
+  await mockAaveLendingPool.setReserveNormalizedIncome(
+    mockERC20Deploy.address,
+    BigNumber.from(2).pow(27)
+  );
+};
+func.tags = ["Mocks"];
+export default func;
