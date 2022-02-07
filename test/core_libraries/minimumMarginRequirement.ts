44665d731ded4acddf7c0493359823177bfb1fef
arturbegyan <arturbegyan98@gmail.com>
2022-02-08 01:29:11 +0300

    trader margin req test

--- test/core_libraries/minimumMarginRequirement.ts
+++ test/core_libraries/minimumMarginRequirement.ts
@@ -1,11 +1,8 @@
-import { Wallet, BigNumber, utils} from "ethers";
+import { Wallet, utils } from "ethers";
 import { expect } from "chai";
-import { ethers, waffle} from "hardhat";
+import { ethers, waffle } from "hardhat";
 import { toBn } from "evm-bn";
-import {
-  fixedAndVariableMathFixture,
-  marginCalculatorFixture,
-} from "../shared/fixtures";
+import { marginCalculatorFixture } from "../shared/fixtures";
 import {
   APY_UPPER_MULTIPLIER,
   APY_LOWER_MULTIPLIER,
@@ -17,16 +14,12 @@ import {
   XI_UPPER,
   XI_LOWER,
   T_MAX,
-  expandTo18Decimals,
+  encodeSqrtRatioX96,
 } from "../shared/utilities";
 
 import { MarginCalculatorTest } from "../../typechain/MarginCalculatorTest";
 import { getCurrentTimestamp } from "../helpers/time";
-import { FixedAndVariableMathTest } from "../../typechain";
 import { TickMath } from "../shared/tickMath";
-import { SqrtPriceMath } from "../shared/sqrtPriceMath";
-import JSBI from "jsbi";
-import { add, mul } from "../shared/functions";
 
 const createFixtureLoader = waffle.createFixtureLoader;
 const { provider } = waffle;
@@ -44,44 +37,97 @@ describe("MarginCalculator", () => {
     loadFixture = createFixtureLoader([wallet, other]);
 
     ({ testMarginCalculator } = await loadFixture(marginCalculatorFixture));
-    
   });
 
   describe("#getAbsoluteFixedTokenDeltaUnbalancedSimulatedUnwind", async () => {
+    // todo, setup an array with a range of inputs and scenarios
 
-    // todo, setup an array with a range of inputs
-
-    // variableTokenDeltaAbsolute,
-    // sqrtRatioCurrX96,
-    // startingFixedRateMultiplierWad,
-    // fixedRateDeviationMinWad,
-    // termEndTimestampWad,
-    // currentTimestampWad,
-    // tMaxWad,
-    // gammaWad,
-    // isFTUnwind
-
-    it("sc1", async () => {
-
+    it("isFTUnwind", async () => {
       const variableTokenDeltaAbsolute = toBn("100");
       const sqrtRatioCurrX96 = TickMath.getSqrtRatioAtTick(0); // price is 1 => fixed rate is 1%
-      console.log("sqrtRatioCurrX96", sqrtRatioCurrX96.toString());
       const startingFixedRateMultiplierWad = toBn("0.5");
       const fixedRateDeviationMinWad = toBn("0.1");
       const termEndTimestampWad = toBn("1675811972"); // approx one year later
       const currentTimestampWad = toBn("1644255020");
-      const tMaxWad = toBn("63113904") // two years
+      const tMaxWad = toBn("63113904"); // two years
       const gammaWad = toBn("1.0");
       const isFTUnwind = true;
-  
-      const realized = await testMarginCalculator.getAbsoluteFixedTokenDeltaUnbalancedSimulatedUnwind(variableTokenDeltaAbsolute, sqrtRatioCurrX96.toString(), startingFixedRateMultiplierWad, fixedRateDeviationMinWad, termEndTimestampWad, currentTimestampWad, tMaxWad, gammaWad, isFTUnwind);
-      console.log("Realized result",  utils.formatEther(realized.toString()));
 
-    })
+      const realized =
+        await testMarginCalculator.getAbsoluteFixedTokenDeltaUnbalancedSimulatedUnwind(
+          variableTokenDeltaAbsolute,
+          sqrtRatioCurrX96.toString(),
+          startingFixedRateMultiplierWad,
+          fixedRateDeviationMinWad,
+          termEndTimestampWad,
+          currentTimestampWad,
+          tMaxWad,
+          gammaWad,
+          isFTUnwind
+        );
+      expect(realized).to.eq(toBn("80.3265329856316712"));
+    });
+  });
 
+  describe("#getMinimumMarginRequirement", async () => {
+    it("sc1", async () => {
+      const margin_engine_params = {
+        apyUpperMultiplierWad: APY_UPPER_MULTIPLIER,
+        apyLowerMultiplierWad: APY_LOWER_MULTIPLIER,
+        minDeltaLMWad: MIN_DELTA_LM,
+        minDeltaIMWad: MIN_DELTA_IM,
+        sigmaSquaredWad: SIGMA_SQUARED,
+        alphaWad: ALPHA,
+        betaWad: BETA,
+        xiUpperWad: XI_UPPER,
+        xiLowerWad: XI_LOWER,
+        tMaxWad: T_MAX,
 
-  })
+        devMulLeftUnwindLMWad: toBn("0.5"),
+        devMulRightUnwindLMWad: toBn("0.5"),
+        devMulLeftUnwindIMWad: toBn("0.8"),
+        devMulRightUnwindIMWad: toBn("0.8"),
 
+        fixedRateDeviationMinLeftUnwindLMWad: toBn("0.1"),
+        fixedRateDeviationMinRightUnwindLMWad: toBn("0.1"),
 
+        fixedRateDeviationMinLeftUnwindIMWad: toBn("0.3"),
+        fixedRateDeviationMinRightUnwindIMWad: toBn("0.3"),
 
+        gammaWad: toBn("1.0"),
+        minMarginToIncentiviseLiquidators: 0, // keep zero for now then do tests with the min liquidator incentive
+      };
+
+      const termStartTimestamp = await getCurrentTimestamp(provider);
+
+      const termStartTimestampWad = toBn(termStartTimestamp.toString());
+
+      const termEndTimestampWad = toBn(
+        (termStartTimestamp + 31556952).toString() // add a year
+      );
+
+      const trader_margin_requirement_params = {
+        fixedTokenBalance: toBn("1000"),
+        variableTokenBalance: toBn("-100"),
+        termStartTimestampWad: termStartTimestampWad,
+        termEndTimestampWad: termEndTimestampWad,
+        isLM: true,
+        historicalApyWad: toBn("0.5"),
+        sqrtPriceX96: encodeSqrtRatioX96(1, 10).toString(),
+        variableFactorWad: toBn("0"),
+      };
+
+      const realized = await testMarginCalculator.getMinimumMarginRequirement(
+        trader_margin_requirement_params,
+        margin_engine_params
+      );
+
+      console.log(
+        "realized minimum trader margin requirement",
+        utils.formatEther(realized.toString())
+      );
+
+      // set up an excel scenario to test this works accordingly
+    });
+  });
 });
