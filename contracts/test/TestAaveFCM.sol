eba1132ffec74687268f1fd098a03e337eeecc69
arturbegyan <arturbegyan98@gmail.com>
2022-02-12 19:50:59 +0300

    linting

--- contracts/test/TestAaveFCM.sol
+++ contracts/test/TestAaveFCM.sol
@@ -4,10 +4,18 @@ pragma solidity ^0.8.0;
 
 import "../AaveFCM.sol";
 
-
 contract TestAaveFCM is AaveFCM {
-
-  constructor (address _underlyingYieldBearingToken, address _vammAddress, address _marginEngineAddress, address _aaveLendingPool) AaveFCM(_underlyingYieldBearingToken, _vammAddress, _marginEngineAddress, _aaveLendingPool)
-  {}
-
+    constructor(
+        address _underlyingYieldBearingToken,
+        address _vammAddress,
+        address _marginEngineAddress,
+        address _aaveLendingPool
+    )
+        AaveFCM(
+            _underlyingYieldBearingToken,
+            _vammAddress,
+            _marginEngineAddress,
+            _aaveLendingPool
+        )
+    {}
 }
