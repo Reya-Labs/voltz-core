a4d27462c61e759af47074fae12269cd4605fbdc
tudor47 <tudorcostin7@gmail.com>
2022-02-13 18:44:35 +0200

    trader margin suspiciously high

--- test/main_contracts/vamm_swaps.ts
+++ test/main_contracts/vamm_swaps.ts
@@ -105,6 +105,20 @@ describe("VAMM", () => {
       xiUpperWad: XI_UPPER,
       xiLowerWad: XI_LOWER,
       tMaxWad: T_MAX,
+
+      devMulLeftUnwindLMWad: toBn("0.5"),
+      devMulRightUnwindLMWad: toBn("0.5"),
+      devMulLeftUnwindIMWad: toBn("0.8"),
+      devMulRightUnwindIMWad: toBn("0.8"),
+
+      fixedRateDeviationMinLeftUnwindLMWad: toBn("0.1"),
+      fixedRateDeviationMinRightUnwindLMWad: toBn("0.1"),
+
+      fixedRateDeviationMinLeftUnwindIMWad: toBn("0.3"),
+      fixedRateDeviationMinRightUnwindIMWad: toBn("0.3"),
+
+      gammaWad: toBn("1.0"),
+      minMarginToIncentiviseLiquidators: 0, // keep zero for now then do tests with the min liquidator incentive
     };
 
     await marginEngineTest.setMarginCalculatorParameters(margin_engine_params);
