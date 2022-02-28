8fee8c91cabd1cd49dcc7d4c92551b1b00b5f8e0
CyclopsRex <cyclopsrex@voltz.xyz>
2022-02-28 21:57:19 +0000

    Deploy rate oracle for existing Aave lending pool.

--- deploy/3.rateOracle.ts
+++ deploy/3.rateOracle.ts
@@ -1,13 +1,43 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+import { config } from "./config";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments;
   const { deployer } = await hre.getNamedAccounts();
   const doLogging = true;
 
-  // TODO: should also be able to read in pre-configured addresses of supported tokens & AaveLendingPools on a given network
+  // If our network has a pre-deployed (e.g. by Aave) lending pool, set up a rate oracle for that
+  const networkContracts = config[network.name];
+  const existingAaveLendingPoolAddress = networkContracts
+    ? networkContracts.aaveLendingPool
+    : null;
+  const testToken = networkContracts ? networkContracts.testToken : null;
+  if (existingAaveLendingPoolAddress) {
+    const aaveLendingPool = await ethers.getContractAt(
+      "IAaveV2LendingPool",
+      existingAaveLendingPoolAddress
+    );
+    const normalizedIncome = await aaveLendingPool.getReserveNormalizedIncome(
+      testToken
+    );
+
+    if (!normalizedIncome) {
+      console.error(
+        `Could not find data for token ${testToken} in Aaave contract ${aaveLendingPool.address}. Ignorning.`
+      );
+    } else {
+      const rateOracleDeploy = await deploy("AaveRateOracle", {
+        from: deployer,
+        args: [aaveLendingPool.address, testToken],
+        log: doLogging,
+      });
+      console.log(
+        `Created rate oracle for Aave pool ${aaveLendingPool.address}`
+      );
+    }
+  }
 
   // Deploy rate oracle pointing at mocks, if mocks exist
   const mockToken = await ethers.getContractOrNull("ERC20Mock");
@@ -18,12 +48,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     console.log(
       `Deploy rate oracle for mocked {token, aave}: {${mockToken.address}, ${mockAaveLendingPool.address}}`
     );
-    await deploy("TestRateOracle", {
+    await deploy("AaveRateOracle", {
       from: deployer,
       args: [mockAaveLendingPool.address, mockToken.address],
       log: doLogging,
     });
   }
+
+  // TODO: should probably also grow bugger of all deployed rate oracles, and read first data point
 };
 func.tags = ["RateOracles"];
 export default func;
