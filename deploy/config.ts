ec8abe4e83e9cdf4c1b542fe83c6e2cd7b3bacb6
CyclopsRex <cyclopsrex@voltz.xyz>
2022-03-23 21:01:08 +0000

    Make IRS config settable per network.
    (In future we may want some of this config settable per-underlying-token.)

--- deploy/config.ts
+++ deploy/config.ts
@@ -1,4 +1,6 @@
+import { BigNumberish } from "ethers";
 import { network } from "hardhat";
+import { toBn } from "../test/helpers/toBn";
 
 // Manage addresses for third-party contracts
 interface TokenConfig {
@@ -6,6 +8,42 @@ interface TokenConfig {
   address: string;
   rateOracleBufferSize: number;
   minSecondsSinceLastUpdate: number;
+}
+
+interface MarginCalculatorParameters {
+  apyUpperMultiplierWad: BigNumberish;
+  apyLowerMultiplierWad: BigNumberish;
+  sigmaSquaredWad: BigNumberish;
+  alphaWad: BigNumberish;
+  betaWad: BigNumberish;
+  xiUpperWad: BigNumberish;
+  xiLowerWad: BigNumberish;
+  tMaxWad: BigNumberish;
+  devMulLeftUnwindLMWad: BigNumberish;
+  devMulRightUnwindLMWad: BigNumberish;
+  devMulLeftUnwindIMWad: BigNumberish;
+  devMulRightUnwindIMWad: BigNumberish;
+  fixedRateDeviationMinLeftUnwindLMWad: BigNumberish;
+  fixedRateDeviationMinRightUnwindLMWad: BigNumberish;
+  fixedRateDeviationMinLeftUnwindIMWad: BigNumberish;
+  fixedRateDeviationMinRightUnwindIMWad: BigNumberish;
+  gammaWad: BigNumberish;
+  minMarginToIncentiviseLiquidators: BigNumberish;
+}
+
+interface IrsConfig {
+  marginEngineSecondsAgo: BigNumberish;
+  marginEngineCacheMaxAgeInSeconds: BigNumberish;
+  marginEngineLiquidatorRewardWad: BigNumberish;
+  marginEngineCalculatorParameters: MarginCalculatorParameters;
+  vammFeeProtocol: BigNumberish;
+  vammFeeWad: BigNumberish;
+  rateOracleBufferSize: BigNumberish;
+}
+
+interface ConfigDefaults extends IrsConfig {
+  rateOracleBufferSize: number;
+  rateOracleMinSecondsSinceLastUpdate: number;
 }
 interface ContractsConfig {
   aaveLendingPool: string;
@@ -18,6 +56,38 @@ interface ContractsConfigMap {
 function duplicateExists(arr: string[]) {
   return new Set(arr).size !== arr.length;
 }
+
+const marginCalculatorDefaults1 = {
+  apyUpperMultiplierWad: toBn(1.5),
+  apyLowerMultiplierWad: toBn(0.7),
+  sigmaSquaredWad: toBn(0.01),
+  alphaWad: toBn(0.04),
+  betaWad: toBn(1),
+  xiUpperWad: toBn(2),
+  xiLowerWad: toBn(1.5),
+  tMaxWad: toBn(31536000), // one year
+  devMulLeftUnwindLMWad: toBn(0.5),
+  devMulRightUnwindLMWad: toBn(0.5),
+  devMulLeftUnwindIMWad: toBn(0.8),
+  devMulRightUnwindIMWad: toBn(0.8),
+  fixedRateDeviationMinLeftUnwindLMWad: toBn(0.1),
+  fixedRateDeviationMinRightUnwindLMWad: toBn(0.1),
+  fixedRateDeviationMinLeftUnwindIMWad: toBn(0.3),
+  fixedRateDeviationMinRightUnwindIMWad: toBn(0.3),
+  gammaWad: toBn(1),
+  minMarginToIncentiviseLiquidators: 0,
+};
+
+export const configDefaults: ConfigDefaults = {
+  marginEngineSecondsAgo: 1209600, // 2 weeks
+  marginEngineCacheMaxAgeInSeconds: 6 * 60 * 60, // 6 hours
+  marginEngineLiquidatorRewardWad: toBn(0.5),
+  marginEngineCalculatorParameters: marginCalculatorDefaults1,
+  vammFeeProtocol: 10,
+  vammFeeWad: toBn(0.001), // 0.1%
+  rateOracleBufferSize: 100,
+  rateOracleMinSecondsSinceLastUpdate: 6 * 60 * 60, // 6 hours
+};
 
 const config: ContractsConfigMap = {
   kovan: {
