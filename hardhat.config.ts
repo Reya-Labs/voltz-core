634fcfe883622bb02e1a3fb9a17276ff2ec43c53
CyclopsRex <cyclopsrex@voltz.xyz>
2022-03-28 20:29:01 +0100

    Add tasks for writing to and querying rate oracle.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,6 +36,7 @@ loadModuleIgnoreErrors("./tasks/updatePositionMargin");
 loadModuleIgnoreErrors("./tasks/increaseObservationCardinalityNext");
 loadModuleIgnoreErrors("./tasks/advanceTimeAndBlock");
 loadModuleIgnoreErrors("./tasks/updateAPYFor15Days");
+loadModuleIgnoreErrors("./tasks/rateOracle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
---
+++ tasks/rateOracle.ts
@@ -0,0 +1,57 @@
+import { task } from "hardhat/config";
+import { utils } from "ethers";
+import { toBn } from "../test/helpers/toBn";
+import { BaseRateOracle } from "../typechain";
+
+task(
+  "writeRateOracle",
+  "Writes a new datapoint for a named rate oracle instance"
+)
+  .addParam(
+    "rateOracle",
+    "The name of the rate oracle as defined in deployments/<network> (e.g. 'AaveRateOracle_USDT'"
+  )
+  .setAction(async (taskArgs, hre) => {
+    const rateOracle = (await hre.ethers.getContract(
+      taskArgs.rateOracle
+    )) as BaseRateOracle;
+    // console.log(`Listing Rates known by Rate Oracle ${rateOracle.address}`);
+
+    await rateOracle.writeOracleEntry();
+  });
+
+task(
+  "queryRateOracle",
+  "Outputs the observations stored within a named rate oracle instance"
+)
+  .addParam(
+    "rateOracle",
+    "The name of the rate oracle as defined in deployments/<network> (e.g. 'AaveRateOracle_USDT'"
+  )
+  .setAction(async (taskArgs, hre) => {
+    const rateOracle = (await hre.ethers.getContract(
+      taskArgs.rateOracle
+    )) as BaseRateOracle;
+    // console.log(`Listing Rates known by Rate Oracle ${rateOracle.address}`);
+
+    const oracleVars = await rateOracle.oracleVars();
+    let csvOutput = `timestamp,value,rawTimestamp,rawValue`;
+
+    for (let i = 0; i <= oracleVars.rateIndex; i++) {
+      const observation = await rateOracle.observations(i);
+      const observationTimeString = new Date(
+        observation.blockTimestamp * 1000
+      ).toISOString();
+      const observedValue = utils.formatUnits(observation.observedValue, 27);
+
+      if (!observation.initialized) {
+        throw new Error(
+          `Error reading data from oracle buffer at position ${i}`
+        );
+      }
+      csvOutput += `\n${observationTimeString},${observedValue},${observation.blockTimestamp},${observation.observedValue}`;
+    }
+    console.log(csvOutput);
+  });
+
+module.exports = {};
