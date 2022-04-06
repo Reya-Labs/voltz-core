7be97ac6ca90d86ad4c478e7109a8bcd6ed77602
tudor47 <tudorcostin7@gmail.com>
2022-04-06 16:27:39 +0300

    ignored json objects from linting

--- test/end_to_end/general_setup/scenario14/scenario14.ts
+++ test/end_to_end/general_setup/scenario14/scenario14.ts
@@ -1,4 +1,4 @@
-import { BigNumber, utils } from "ethers";
+import { utils } from "ethers";
 import { toBn } from "evm-bn";
 import { consts } from "../../../helpers/constants";
 import { advanceTimeAndBlock } from "../../../helpers/time";
