0822aba12152868244bf0f5bb739b38ba2fc1ad8
Artur Begyan <arturbegyan98@gmail.com>
2022-04-08 22:48:42 +0100

    linting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -93,7 +93,7 @@ const config: HardhatUserConfig = {
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 
   namedAccounts: {
