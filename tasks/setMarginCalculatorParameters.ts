3358d2cbaae6876162fb1b40c20130d830898ae6
Artur Begyan <arturbegyan98@gmail.com>
2022-04-11 21:04:43 +0100

    linting

--- tasks/setMarginCalculatorParameters.ts
+++ tasks/setMarginCalculatorParameters.ts
@@ -1,28 +1,25 @@
 import { task } from "hardhat/config";
 import { MarginEngine } from "../typechain";
-import {
-    getConfigDefaults,
-  } from "../deployConfig/config";
+import { getConfigDefaults } from "../deployConfig/config";
 
-
-task("setMarginCalculatorParameters", "Sets Margin Calculator Parameters").setAction(async (_, hre) => {
-
+task(
+  "setMarginCalculatorParameters",
+  "Sets Margin Calculator Parameters"
+).setAction(async (_, hre) => {
   // todo: make settable in the createIrsInstance task
   const marginEngineAddress = "0xdcf2d0e379c29f67df42f6b720591ae66da48e3c";
 
-    const marginEngine = (await hre.ethers.getContractAt(
-        "MarginEngine",
-        marginEngineAddress
-    )) as MarginEngine;
+  const marginEngine = (await hre.ethers.getContractAt(
+    "MarginEngine",
+    marginEngineAddress
+  )) as MarginEngine;
 
-    const configDefaults = getConfigDefaults(hre.network.name);
-    let trx = await marginEngine.setMarginCalculatorParameters(
-    
-        configDefaults.marginEngineCalculatorParameters
-    );
+  const configDefaults = getConfigDefaults(hre.network.name);
+  const trx = await marginEngine.setMarginCalculatorParameters(
+    configDefaults.marginEngineCalculatorParameters
+  );
 
-    await trx.wait();
-
+  await trx.wait();
 });
 
 module.exports = {};
