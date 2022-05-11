67b61b313182628eb9d9e5e8475ac9aee95328de
tudor47 <tudorcostin7@gmail.com>
2022-05-11 18:11:19 +0100

    feat: start of fuzzing

--- contracts/test/E2ESetup.sol
+++ contracts/test/E2ESetup.sol
@@ -67,24 +67,6 @@ contract E2ESetup is CustomErrors {
         public positionSwapsHistory;
     mapping(bytes32 => uint256) public sizeOfPositionSwapsHistory;
 
-    int256 public initialCashflow = 0;
-    int256 public liquidationRewards = 0;
-    int256 public fcmFees = 0;
-
-    uint256 public keepInMindGas;
-
-    // function getReserveNormalizedIncome() internal view returns (uint256) {
-    //     IRateOracle rateOracle = IMarginEngine(MEAddress).rateOracle();
-    //     IAaveV2LendingPool aaveLendingPool = IAaveV2LendingPool(
-    //         IAaveRateOracle(address(rateOracle)).aaveLendingPool()
-    //     );
-    //     uint256 reserveNormalizedIncome = aaveLendingPool
-    //         .getReserveNormalizedIncome(
-    //             IMarginEngine(MEAddress).underlyingToken()
-    //         );
-    //     return reserveNormalizedIncome;
-    // }
-
     function addSwapSnapshot(
         address owner,
         int24 tickLower,
@@ -108,8 +90,6 @@ contract E2ESetup is CustomErrors {
             PRBMathUD60x18.fromUint(100)
         );
 
-        // get the current reserve normalized income from Aave
-        // uint256 reserveNormalizedIncome = getReserveNormalizedIncome();
         uint256 reserveNormalizedIncome = 1;
 
         SwapSnapshot memory swapSnapshot = SwapSnapshot({
@@ -204,10 +184,6 @@ contract E2ESetup is CustomErrors {
         uint256 MEBalanceAfter = IERC20Minimal(
             IMarginEngine(MEAddress).underlyingToken()
         ).balanceOf(MEAddress);
-
-        fcmFees += int256(MEBalanceAfter) - int256(MEBalanceBefore);
-
-        continuousInvariants();
     }
 
     function unwindFullyCollateralisedFixedTakerSwap(
@@ -230,10 +206,6 @@ contract E2ESetup is CustomErrors {
         uint256 MEBalanceAfter = IERC20Minimal(
             IMarginEngine(MEAddress).underlyingToken()
         ).balanceOf(MEAddress);
-
-        fcmFees += int256(MEBalanceAfter) - int256(MEBalanceBefore);
-
-        continuousInvariants();
     }
 
     function settleYBATrader(address trader) external {
@@ -299,7 +271,6 @@ contract E2ESetup is CustomErrors {
     ) public returns (int256 positionMarginRequirement) {
         this.addPosition(recipient, tickLower, tickUpper);
 
-        uint256 gasBefore = gasleft();
         positionMarginRequirement = Actor(recipient).mintViaAMM(
             VAMMAddress,
             recipient,
@@ -307,9 +278,6 @@ contract E2ESetup is CustomErrors {
             tickUpper,
             amount
         );
-        keepInMindGas = gasBefore - gasleft();
-
-        continuousInvariants();
     }
 
     function burnViaAMM(
@@ -320,7 +288,6 @@ contract E2ESetup is CustomErrors {
     ) public {
         this.addPosition(recipient, tickLower, tickUpper);
 
-        uint256 gasBefore = gasleft();
         Actor(recipient).burnViaAMM(
             VAMMAddress,
             recipient,
@@ -328,9 +295,6 @@ contract E2ESetup is CustomErrors {
             tickUpper,
             amount
         );
-        keepInMindGas = gasBefore - gasleft();
-
-        continuousInvariants();
     }
 
     function swapViaAMM(IVAMM.SwapParams memory params)
@@ -345,7 +309,6 @@ contract E2ESetup is CustomErrors {
     {
         this.addPosition(params.recipient, params.tickLower, params.tickUpper);
 
-        uint256 gasBefore = gasleft();
         (
             _fixedTokenDelta,
             _variableTokenDelta,
@@ -353,9 +316,6 @@ contract E2ESetup is CustomErrors {
             _fixedTokenDeltaUnbalanced,
             _marginRequirement
         ) = Actor(params.recipient).swapViaAMM(VAMMAddress, params);
-        keepInMindGas = gasBefore - gasleft();
-
-        continuousInvariants();
 
         this.addSwapSnapshot(
             params.recipient,
@@ -397,12 +357,6 @@ contract E2ESetup is CustomErrors {
             liquidatorBalanceBefore <= liquidatorBalanceAfter,
             "liquidation reward should be positive"
         );
-
-        liquidationRewards +=
-            int256(liquidatorBalanceAfter) -
-            int256(liquidatorBalanceBefore);
-
-        continuousInvariants();
     }
 
     function setIntegrationApproval(
@@ -417,30 +371,6 @@ contract E2ESetup is CustomErrors {
         );
     }
 
-    function estimatedVariableFactorFromStartToMaturity()
-        internal
-        returns (uint256 _estimatedVariableFactorFromStartToMaturity)
-    {
-        uint256 historicalAPYWad = IMarginEngine(MEAddress).getHistoricalApy();
-
-        uint256 termStartTimestampWad = IMarginEngine(MEAddress)
-            .termStartTimestampWad();
-        uint256 termEndTimestampWad = IMarginEngine(MEAddress)
-            .termEndTimestampWad();
-
-        uint256 termInYears = FixedAndVariableMath.accrualFact(
-            termEndTimestampWad - termStartTimestampWad
-        );
-
-        // calculate the estimated variable factor from start to maturity
-        _estimatedVariableFactorFromStartToMaturity =
-            PRBMathUD60x18.pow(
-                (PRBMathUD60x18.fromUint(1) + historicalAPYWad),
-                termInYears
-            ) -
-            PRBMathUD60x18.fromUint(1);
-    }
-
     function updatePositionMarginViaAMM(
         address _owner,
         int24 tickLower,
@@ -449,7 +379,6 @@ contract E2ESetup is CustomErrors {
     ) public {
         this.addPosition(_owner, tickLower, tickUpper);
 
-        uint256 gasBefore = gasleft();
         Actor(_owner).updatePositionMarginViaAMM(
             MEAddress,
             _owner,
@@ -457,323 +386,6 @@ contract E2ESetup is CustomErrors {
             tickUpper,
             marginDelta
         );
-        keepInMindGas = gasBefore - gasleft();
-        initialCashflow += marginDelta;
-
-        if (
-            PRBMathUD60x18.fromUint(block.timestamp) <
-            IMarginEngine(MEAddress).termEndTimestampWad()
-        ) continuousInvariants();
-    }
-
-    // function computeSettlementCashflowForSwapSnapshot(
-    //     SwapSnapshot memory snapshot
-    // ) internal view returns (int256 settlementCashflow) {
-    //     // calculate the variable factor for the period the swap was active
-    //     // needs to be called at the same time as the term end timestamp, otherwise need to cache the reserve normalised income for the term end timestamp in the E2E setup
-
-    //     uint256 reserveNormalizedIncomeRay = getReserveNormalizedIncome();
-    //     uint256 reserveNormalizedIncomeAtSwapInceptionRay = snapshot
-    //         .reserveNormalizedIncomeAtSwap;
-    //     uint256 variableFactorFromSwapInceptionToMaturityWad = WadRayMath
-    //         .rayToWad(
-    //             WadRayMath
-    //                 .rayDiv(
-    //                     reserveNormalizedIncomeRay,
-    //                     reserveNormalizedIncomeAtSwapInceptionRay
-    //                 )
-    //                 .sub(WadRayMath.RAY)
-    //         );
-
-    //     // swapInitiationTimestampWad
-    //     uint256 termEndTimestampWad = IMarginEngine(MEAddress)
-    //         .termEndTimestampWad();
-
-    //     uint256 timeInSecondsBetweenSwapInitiationAndMaturityWad = termEndTimestampWad -
-    //             snapshot.swapInitiationTimestampWad;
-    //     uint256 timeInYearsWad = FixedAndVariableMath.accrualFact(
-    //         timeInSecondsBetweenSwapInitiationAndMaturityWad
-    //     );
-
-    //     uint256 fixedFactorWad = PRBMathUD60x18.mul(
-    //         snapshot.fixedRateWad,
-    //         timeInYearsWad
-    //     );
-
-    //     int256 variableFixedFactorDelta;
-    //     if (snapshot.isFT) {
-    //         variableFixedFactorDelta =
-    //             int256(fixedFactorWad) -
-    //             int256(variableFactorFromSwapInceptionToMaturityWad);
-    //     } else {
-    //         variableFixedFactorDelta =
-    //             int256(variableFactorFromSwapInceptionToMaturityWad) -
-    //             int256(fixedFactorWad);
-    //     }
-
-    //     int256 settlementCashflowWad = PRBMathSD59x18.mul(
-    //         int256(snapshot.notional),
-    //         variableFixedFactorDelta
-    //     );
-    //     settlementCashflow = PRBMathSD59x18.toInt(settlementCashflowWad);
-    // }
-
-    // function settlementCashflowBasedOnSwapSnapshots(
-    //     address _owner,
-    //     int24 tickLower,
-    //     int24 tickUpper
-    // ) public view returns (int256) {
-    //     (
-    //         SwapSnapshot[] memory snapshots,
-    //         uint256 len
-    //     ) = getPositionSwapsHistory(_owner, tickLower, tickUpper);
-
-    //     int256 settlementCashflow;
-
-    //     for (uint256 i = 0; i < len; i++) {
-    //         settlementCashflow += computeSettlementCashflowForSwapSnapshot(
-    //             snapshots[i]
-    //         );
-    //     }
-
-    //     return settlementCashflow;
-    // }
-
-    // function invariantPostMaturity() public {
-    //     // calculate the cashflows for each position based on their swap snapshots and based on their fixed and variable token balances
-    //     // this only works for Positins that have not minted liquidity since their settlementCashflow is also a function of trades in their tick range
-    //     // assume in this scenarios all the swapper only swap
-
-    //     uint256 termStartTimestampWad = uint256(
-    //         IMarginEngine(MEAddress).termStartTimestampWad()
-    //     );
-    //     uint256 termEndTimestampWad = uint256(
-    //         IMarginEngine(MEAddress).termEndTimestampWad()
-    //     );
-
-    //     for (uint256 i = 1; i <= sizeAllPositions; i++) {
-    //         uint256 variableFactor = IRateOracle(rateOracleAddress)
-    //             .variableFactor(termStartTimestampWad, termEndTimestampWad);
-
-    //         TestMarginEngine(MEAddress)
-    //             .updatePositionTokenBalancesAndAccountForFeesTest(
-    //                 allPositions[i].owner,
-    //                 allPositions[i].tickLower,
-    //                 allPositions[i].tickUpper,
-    //                 false
-    //             );
-
-    //         Position.Info memory position = IMarginEngine(MEAddress)
-    //             .getPosition(
-    //                 allPositions[i].owner,
-    //                 allPositions[i].tickLower,
-    //                 allPositions[i].tickUpper
-    //             );
-
-    //         int256 settlementCashflow = FixedAndVariableMath
-    //             .calculateSettlementCashflow(
-    //                 position.fixedTokenBalance,
-    //                 position.variableTokenBalance,
-    //                 termStartTimestampWad,
-    //                 termEndTimestampWad,
-    //                 variableFactor
-    //             );
-
-    //         int256 settlementCashflowSS = settlementCashflowBasedOnSwapSnapshots(
-    //                 allPositions[i].owner,
-    //                 allPositions[i].tickLower,
-    //                 allPositions[i].tickUpper
-    //             );
-
-    //         int256 approximation = 100000;
-
-    //         int256 delta = settlementCashflow - settlementCashflowSS;
-
-    //         require(
-    //             abs(delta) < uint256(approximation),
-    //             "settlement cashflows from swap snapshots"
-    //         );
-    //     }
-    // }
-
-    function continuousInvariants() public {
-        int256 totalFixedTokens = 0;
-        int256 totalVariableTokens = 0;
-        int256 totalCashflow = 0;
-
-        uint256 termStartTimestampWad = uint256(
-            IMarginEngine(MEAddress).termStartTimestampWad()
-        );
-        uint256 termEndTimestampWad = uint256(
-            IMarginEngine(MEAddress).termEndTimestampWad()
-        );
-
-        int256 liquidatablePositions = 0;
-        for (uint256 i = 1; i <= sizeAllPositions; i++) {
-            TestMarginEngine(MEAddress)
-                .updatePositionTokenBalancesAndAccountForFeesTest(
-                    allPositions[i].owner,
-                    allPositions[i].tickLower,
-                    allPositions[i].tickUpper,
-                    false
-                );
-
-            Position.Info memory position = IMarginEngine(MEAddress)
-                .getPosition(
-                    allPositions[i].owner,
-                    allPositions[i].tickLower,
-                    allPositions[i].tickUpper
-                );
-
-            // Printer.printInt256(
-            //     "   fixedTokenBalance:",
-            //     position.fixedTokenBalance
-            // );
-            // Printer.printInt256(
-            //     "variableTokenBalance:",
-            //     position.variableTokenBalance
-            // );
-            // Printer.printInt256("              margin:", position.margin);
-
-            int256 estimatedSettlementCashflow = FixedAndVariableMath
-                .calculateSettlementCashflow(
-                    position.fixedTokenBalance,
-                    position.variableTokenBalance,
-                    termStartTimestampWad,
-                    termEndTimestampWad,
-                    estimatedVariableFactorFromStartToMaturity()
-                );
-
-            TestMarginEngine(MEAddress).getPositionMarginRequirementTest(
-                allPositions[i].owner,
-                allPositions[i].tickLower,
-                allPositions[i].tickUpper,
-                true
-            );
-            uint256 marginRequirement = TestMarginEngine(MEAddress).getMargin();
-            // Printer.printUint256("  margin requirement:", marginRequirement);
-
-            if (int256(marginRequirement) > position.margin) {
-                liquidatablePositions += 1;
-            }
-
-            bytes32 hashedPositon = keccak256(
-                abi.encodePacked(
-                    allPositions[i].owner,
-                    allPositions[i].tickLower,
-                    allPositions[i].tickUpper
-                )
-            );
-
-            PositionSnapshot memory positionSnapshot;
-
-            positionSnapshot.margin = position.margin;
-            positionSnapshot.marginRequirement = marginRequirement;
-
-            positionSnapshot.termStartTimestampWad = termStartTimestampWad;
-            positionSnapshot.termEndTimestampWad = termEndTimestampWad;
-            positionSnapshot.currentTimestampWad = Time.blockTimestampScaled();
-
-            positionSnapshot
-                .estimatedSettlementCashflow = estimatedSettlementCashflow;
-
-            positionSnapshot.fixedTokenBalance = position.fixedTokenBalance;
-            positionSnapshot.variableTokenBalance = position
-                .variableTokenBalance;
-
-            sizeOfPositionHistory[hashedPositon] += 1;
-            positionHistory[hashedPositon][
-                sizeOfPositionHistory[hashedPositon]
-            ] = positionSnapshot;
-
-            totalFixedTokens += position.fixedTokenBalance;
-            totalVariableTokens += position.variableTokenBalance;
-            totalCashflow += position.margin;
-            totalCashflow += estimatedSettlementCashflow;
-
-            // Printer.printInt256(
-            //     "              esc:",
-            //     estimatedSettlementCashflow
-            // );
-        }
-
-        for (uint256 i = 1; i <= sizeAllYBATraders; i++) {
-            TraderWithYieldBearingAssets.Info memory trader = IFCM(FCMAddress)
-                .getTraderWithYieldBearingAssets(allYBATraders[i]);
-            totalFixedTokens += trader.fixedTokenBalance;
-            totalVariableTokens += trader.variableTokenBalance;
-
-            int256 estimatedSettlementCashflow = FixedAndVariableMath
-                .calculateSettlementCashflow(
-                    trader.fixedTokenBalance,
-                    int256(trader.variableTokenBalance),
-                    termStartTimestampWad,
-                    termEndTimestampWad,
-                    estimatedVariableFactorFromStartToMaturity()
-                );
-
-            totalCashflow += estimatedSettlementCashflow;
-
-            // Printer.printInt256(
-            //     "   fixedTokenBalance:",
-            //     trader.fixedTokenBalance
-            // );
-            // Printer.printInt256(
-            //     "variableTokenBalance:",
-            //     trader.variableTokenBalance
-            // );
-            // Printer.printInt256(
-            //     "              YBA esc:",
-            //     estimatedSettlementCashflow
-            // );
-        }
-
-        totalCashflow += int256(IVAMM(VAMMAddress).protocolFees());
-        totalCashflow += int256(liquidationRewards);
-        totalCashflow -= fcmFees;
-
-        // Printer.printInt256("fcmFees", fcmFees);
-        // Printer.printInt256("   totalFixedTokens:", totalFixedTokens);
-        // Printer.printInt256("totalVariableTokens:", totalVariableTokens);
-        // Printer.printInt256("   initialCashflow:", initialCashflow);
-        // Printer.printInt256(
-        //     "      deltaCashflow:",
-        //     totalCashflow - initialCashflow
-        // );
-        // Printer.printInt256("liquidatable Positions", liquidatablePositions);
-        // Printer.printEmptyLine();
-
-        // ideally, this should be 0
-        int256 approximation = 100000;
-
-        // Printer.printUint256("      app:", uint256(approximation));
-
-        require(
-            -approximation < totalFixedTokens && totalFixedTokens <= 0,
-            "fixed tokens don't net out"
-        );
-        require(
-            -approximation < totalVariableTokens && totalVariableTokens <= 0,
-            "variable tokens don't net out"
-        );
-        require(
-            initialCashflow >= totalCashflow &&
-                totalCashflow > initialCashflow - approximation,
-            "system loss: undercollateralized"
-        );
-
-        // require(
-        //     abs(totalFixedTokens) < uint256(approximation),
-        //     "fixed tokens don't net out"
-        // );
-        // require(
-        //     abs(totalVariableTokens) < uint256(approximation),
-        //     "variable tokens don't net out"
-        // );
-        // require(
-        //     abs(totalCashflow - initialCashflow) < uint256(approximation),
-        //     "cashflows don't net out"
-        // );
     }
 
     function getPositionSwapsHistory(
