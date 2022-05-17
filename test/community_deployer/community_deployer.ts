9f52daf8ed14414d5b7a32bcfd6259241025ce72
Artur Begyan <arturbegyan98@gmail.com>
2022-05-17 22:41:21 +0100

    linting

--- test/community_deployer/community_deployer.ts
+++ test/community_deployer/community_deployer.ts
@@ -9,9 +9,11 @@ import BalanceTree from "./balance-tree";
 
 /// CONSTANTS
 const MASTER_VAMM_ADDRESS = "0x067232D22d5bb8DC7cDaBa5A909ac8b089539462"; // dummy value
-const MASTER_MARGIN_ENGINE_ADDRESS = "0x067232D22d5bb8DC7cDaBa5A909ac8b089539462"; // dummy value
+const MASTER_MARGIN_ENGINE_ADDRESS =
+  "0x067232D22d5bb8DC7cDaBa5A909ac8b089539462"; // dummy value
 const QUORUM_VOTES = 1;
-const ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+const ZERO_BYTES32 =
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 describe("CommunityDeployer", () => {
   // below tests work under the assumption that the quorum is 1
@@ -23,111 +25,106 @@ describe("CommunityDeployer", () => {
   let wallet: Wallet, other: Wallet;
 
   beforeEach(async () => {
-
     // deploy mock genesis nft
-    const mockGenesisNFTFactory = await ethers.getContractFactory("MockGenesisNFT");
+    const mockGenesisNFTFactory = await ethers.getContractFactory(
+      "MockGenesisNFT"
+    );
     mockGenesisNFT = (await mockGenesisNFTFactory.deploy()) as MockGenesisNFT;
     [wallet, other] = await (ethers as any).getSigners();
-    
+
     // deploy community deployer
     const communityDeployerFactory = await ethers.getContractFactory(
       "CommunityDeployer"
     );
 
-    communityDeployer =
-      (await communityDeployerFactory.deploy(
-        MASTER_VAMM_ADDRESS,
-        MASTER_MARGIN_ENGINE_ADDRESS,
-        mockGenesisNFT.address,
-        QUORUM_VOTES,
-        wallet.address,
-        ZERO_BYTES32
-      )) as CommunityDeployer;
+    communityDeployer = (await communityDeployerFactory.deploy(
+      MASTER_VAMM_ADDRESS,
+      MASTER_MARGIN_ENGINE_ADDRESS,
+      mockGenesisNFT.address,
+      QUORUM_VOTES,
+      wallet.address,
+      ZERO_BYTES32
+    )) as CommunityDeployer;
   });
 
   it("merkle root: returns zero merkle root", async () => {
     expect(await communityDeployer.merkleRoot()).to.eq(ZERO_BYTES32);
-  })
+  });
 
   it("fails to cast for empty proof", async () => {
     await expect(communityDeployer.castVote(0, 1, true, [])).to.be.revertedWith(
       "invalid merkle proof"
     );
-  })
+  });
 
   describe("two account tree", () => {
     let tree: BalanceTree;
 
     beforeEach("setup: two account tree", async () => {
       tree = new BalanceTree([
-        { account: wallet.address, amount: BigNumber.from(100)},
-        { account: other.address, amount: BigNumber.from(101)},
+        { account: wallet.address, amount: BigNumber.from(100) },
+        { account: other.address, amount: BigNumber.from(101) },
       ]);
 
       it("successful vote", async () => {
         const proof0 = tree.getProof(0, wallet.address, BigNumber.from(100));
-        
-        await expect(communityDeployer.connect(wallet).castVote(0, 100, true, proof0))
-        .to.emit(communityDeployer, 'Voted')
-        .withArgs(0, wallet.address, 100); 
+
+        await expect(
+          communityDeployer.connect(wallet).castVote(0, 100, true, proof0)
+        )
+          .to.emit(communityDeployer, "Voted")
+          .withArgs(0, wallet.address, 100);
 
         const proof1 = tree.getProof(0, other.address, BigNumber.from(101));
-        
-        await expect(communityDeployer.connect(other).castVote(1, 101, true, proof1))
-        .to.emit(communityDeployer, 'Voted')
-        .withArgs(1, other.address, 101); 
 
-      })
+        await expect(
+          communityDeployer.connect(other).castVote(1, 101, true, proof1)
+        )
+          .to.emit(communityDeployer, "Voted")
+          .withArgs(1, other.address, 101);
+      });
 
       it("casts the vote", async () => {
-
         const proof0 = tree.getProof(0, wallet.address, BigNumber.from(100));
         expect(await communityDeployer.yesVoteCount()).to.eq(0);
         await communityDeployer.castVote(0, 100, true, proof0);
         expect(await communityDeployer.yesVoteCount()).to.eq(100);
-
-      })
+      });
 
       it("sets hasVoted", async () => {
-
         const proof0 = tree.getProof(0, wallet.address, BigNumber.from(100));
         expect(await communityDeployer.hasVoted(0)).to.eq(false);
         expect(await communityDeployer.hasVoted(1)).to.eq(false);
         await communityDeployer.connect(wallet).castVote(0, 100, true, proof0);
         expect(await communityDeployer.hasVoted(0)).to.eq(true);
         expect(await communityDeployer.hasVoted(1)).to.eq(false);
-
-      })
+      });
 
       it("cannot allow two votes", async () => {
-
         const proof0 = tree.getProof(0, wallet.address, BigNumber.from(100));
         await communityDeployer.connect(wallet).castVote(0, 100, true, proof0);
-        await expect(communityDeployer.connect(wallet).castVote(0, 100, true, proof0)).to.be.revertedWith(
-          'duplicate vote'
-        );
-
-      })
+        await expect(
+          communityDeployer.connect(wallet).castVote(0, 100, true, proof0)
+        ).to.be.revertedWith("duplicate vote");
+      });
 
       it("correctly casts a yes vote", async () => {
-
         const proof0 = tree.getProof(0, wallet.address, BigNumber.from(100));
-    
+
         await communityDeployer.connect(wallet).castVote(0, 100, true, proof0);
-    
+
         const yesVoteCount = await communityDeployer.yesVoteCount();
         const noVoteCount = await communityDeployer.noVoteCount();
-    
+
         expect(yesVoteCount).to.eq(100);
         expect(noVoteCount).to.eq(0);
       });
-    
-      it("correctly casts a no vote", async () => {
 
+      it("correctly casts a no vote", async () => {
         const proof0 = tree.getProof(0, wallet.address, BigNumber.from(100));
-    
+
         await communityDeployer.connect(wallet).castVote(0, 100, false, proof0);
-    
+
         const yesVoteCount = await communityDeployer.yesVoteCount();
         const noVoteCount = await communityDeployer.noVoteCount();
 
@@ -139,10 +136,9 @@ describe("CommunityDeployer", () => {
         const proof0 = tree.getProof(0, wallet.address, BigNumber.from(100));
 
         await advanceTimeAndBlock(BigNumber.from(172801), 1);
-        await expect(communityDeployer.connect(wallet).castVote(0, 100, false, proof0)).to.be.revertedWith(
-          "voting period over"
-        );
-
+        await expect(
+          communityDeployer.connect(wallet).castVote(0, 100, false, proof0)
+        ).to.be.revertedWith("voting period over");
       });
 
       it("unable to queue if voting period is not over", async () => {
@@ -173,7 +169,9 @@ describe("CommunityDeployer", () => {
         const yesVoteCount = await communityDeployer.yesVoteCount();
         expect(yesVoteCount).to.eq(100); // the quorum is reached
         await advanceTimeAndBlock(BigNumber.from(172801), 1); // make sure the voting period is over
-        await expect(communityDeployer.deploy()).to.be.revertedWith("not queued");
+        await expect(communityDeployer.deploy()).to.be.revertedWith(
+          "not queued"
+        );
       });
 
       it("unable to deploy if timelock period is not over", async () => {
@@ -199,7 +197,10 @@ describe("CommunityDeployer", () => {
         await communityDeployer.deploy();
         const factoryAddress = await communityDeployer.voltzFactory();
         expect(factoryAddress).to.not.eq("0");
-        const factory = await ethers.getContractAt("Factory", factoryAddress) as Factory;
+        const factory = (await ethers.getContractAt(
+          "Factory",
+          factoryAddress
+        )) as Factory;
         const factoryOwner = await factory.owner();
         expect(factoryOwner).to.eq(wallet.address);
         const masterMarginEngineAddress = await factory.masterMarginEngine();
@@ -213,8 +214,6 @@ describe("CommunityDeployer", () => {
       // cannot vote more than proof
       // gas consumption test
       // larger trees
-    })
-
-  })
-
+    });
+  });
 });
--- test/community_deployer/parse-balance-map.ts
+++ test/community_deployer/parse-balance-map.ts
@@ -1,31 +1,33 @@
-import { BigNumber, utils } from 'ethers'
-import BalanceTree from './balance-tree'
+import { BigNumber, utils } from "ethers";
+import BalanceTree from "./balance-tree";
 
-const { isAddress, getAddress } = utils
+const { isAddress, getAddress } = utils;
 
 // This is the blob that gets distributed and pinned to IPFS.
 // It is completely sufficient for recreating the entire merkle tree.
 // Anyone can verify that all air drops are included in the tree,
 // and the tree has no additional distributions.
 interface MerkleDistributorInfo {
-  merkleRoot: string
-  tokenTotal: string
+  merkleRoot: string;
+  tokenTotal: string;
   claims: {
     [account: string]: {
-      index: number
-      amount: string
-      proof: string[]
+      index: number;
+      amount: string;
+      proof: string[];
       flags?: {
-        [flag: string]: boolean
-      }
-    }
-  }
+        [flag: string]: boolean;
+      };
+    };
+  };
 }
 
-type OldFormat = { [account: string]: number | string }
-type NewFormat = { address: string; earnings: string; reasons: string }
+type OldFormat = { [account: string]: number | string };
+type NewFormat = { address: string; earnings: string; reasons: string };
 
-export function parseBalanceMap(balances: OldFormat | NewFormat[]): MerkleDistributorInfo {
+export function parseBalanceMap(
+  balances: OldFormat | NewFormat[]
+): MerkleDistributorInfo {
   // if balances are in an old format, process them
   const balancesInNewFormat: NewFormat[] = Array.isArray(balances)
     ? balances
@@ -33,60 +35,72 @@ export function parseBalanceMap(balances: OldFormat | NewFormat[]): MerkleDistri
         (account): NewFormat => ({
           address: account,
           earnings: `0x${balances[account].toString(16)}`,
-          reasons: '',
+          reasons: "",
         })
-      )
+      );
 
   const dataByAddress = balancesInNewFormat.reduce<{
-    [address: string]: { amount: BigNumber; flags?: { [flag: string]: boolean } }
+    [address: string]: {
+      amount: BigNumber;
+      flags?: { [flag: string]: boolean };
+    };
   }>((memo, { address: account, earnings, reasons }) => {
     if (!isAddress(account)) {
-      throw new Error(`Found invalid address: ${account}`)
+      throw new Error(`Found invalid address: ${account}`);
     }
-    const parsed = getAddress(account)
-    if (memo[parsed]) throw new Error(`Duplicate address: ${parsed}`)
-    const parsedNum = BigNumber.from(earnings)
-    if (parsedNum.lte(0)) throw new Error(`Invalid amount for account: ${account}`)
+    const parsed = getAddress(account);
+    if (memo[parsed]) throw new Error(`Duplicate address: ${parsed}`);
+    const parsedNum = BigNumber.from(earnings);
+    if (parsedNum.lte(0))
+      throw new Error(`Invalid amount for account: ${account}`);
 
     const flags = {
-      isSOCKS: reasons.includes('socks'),
-      isLP: reasons.includes('lp'),
-      isUser: reasons.includes('user'),
-    }
+      isSOCKS: reasons.includes("socks"),
+      isLP: reasons.includes("lp"),
+      isUser: reasons.includes("user"),
+    };
 
-    memo[parsed] = { amount: parsedNum, ...(reasons === '' ? {} : { flags }) }
-    return memo
-  }, {})
+    memo[parsed] = { amount: parsedNum, ...(reasons === "" ? {} : { flags }) };
+    return memo;
+  }, {});
 
-  const sortedAddresses = Object.keys(dataByAddress).sort()
+  const sortedAddresses = Object.keys(dataByAddress).sort();
 
   // construct a tree
   const tree = new BalanceTree(
-    sortedAddresses.map((address) => ({ account: address, amount: dataByAddress[address].amount }))
-  )
+    sortedAddresses.map((address) => ({
+      account: address,
+      amount: dataByAddress[address].amount,
+    }))
+  );
 
   // generate claims
   const claims = sortedAddresses.reduce<{
-    [address: string]: { amount: string; index: number; proof: string[]; flags?: { [flag: string]: boolean } }
+    [address: string]: {
+      amount: string;
+      index: number;
+      proof: string[];
+      flags?: { [flag: string]: boolean };
+    };
   }>((memo, address, index) => {
-    const { amount, flags } = dataByAddress[address]
+    const { amount, flags } = dataByAddress[address];
     memo[address] = {
       index,
       amount: amount.toHexString(),
       proof: tree.getProof(index, address, amount),
       ...(flags ? { flags } : {}),
-    }
-    return memo
-  }, {})
+    };
+    return memo;
+  }, {});
 
   const tokenTotal: BigNumber = sortedAddresses.reduce<BigNumber>(
     (memo, key) => memo.add(dataByAddress[key].amount),
     BigNumber.from(0)
-  )
+  );
 
   return {
     merkleRoot: tree.getHexRoot(),
     tokenTotal: tokenTotal.toHexString(),
     claims,
-  }
+  };
 }
