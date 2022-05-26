3f18cfffc566ea7f99605b68f0a3fa2fc94bbf78
tudor47 <tudorcostin7@gmail.com>
2022-05-26 19:39:47 +0300

    feat: pushed MC into ME

--- contracts/MarginEngine.sol
+++ contracts/MarginEngine.sol
@@ -3,7 +3,6 @@
 pragma solidity =0.8.9;
 import "./core_libraries/Tick.sol";
 import "./interfaces/IMarginEngine.sol";
-import "./core_libraries/MarginCalculator.sol";
 import "./interfaces/rate_oracles/IRateOracle.sol";
 import "./interfaces/fcms/IFCM.sol";
 import "prb-math/contracts/PRBMathUD60x18.sol";
@@ -15,6 +14,7 @@ import "./core_libraries/SafeTransferLib.sol";
 import "./storage/MarginEngineStorage.sol";
 import "./utils/SafeCastUni.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "contracts/utils/SqrtPriceMath.sol";
 
 contract MarginEngine is
     MarginEngineStorage,
@@ -23,6 +23,9 @@ contract MarginEngine is
     VoltzPausable,
     UUPSUpgradeable
 {
+    using PRBMathSD59x18 for int256;
+    using PRBMathUD60x18 for uint256;
+
     using SafeCastUni for uint256;
     using SafeCastUni for int256;
     using Tick for mapping(int24 => Tick.Info);
@@ -32,10 +35,244 @@ contract MarginEngine is
 
     using SafeTransferLib for IERC20Minimal;
 
+    /// @dev Seconds in a year
+    int256 public constant SECONDS_IN_YEAR = 31536000e18;
+
+    uint256 public constant ONE_UINT = 1e18;
+    int256 public constant ONE = 1e18;
+
     uint256 public constant MAX_LOOKBACK_WINDOW_IN_SECONDS = 315360000; // ten years
     uint256 public constant MIN_LOOKBACK_WINDOW_IN_SECONDS = 3600; // one hour
     uint256 public constant MAX_CACHE_MAX_AGE_IN_SECONDS = 1209600; // two weeks
     uint256 public constant MAX_LIQUIDATOR_REWARD_WAD = 3e17; // 30%
+
+    /// @dev In the litepaper the timeFactor is exp(-beta*(t-s)/t_max) where t is the maturity timestamp, and t_max is the max number of seconds for the IRS AMM duration, s is the current timestamp and beta is a diffusion process parameter set via calibration
+    function computeTimeFactor(
+        uint256 termEndTimestampWad,
+        uint256 currentTimestampWad
+    ) internal view returns (int256 timeFactorWad) {
+        require(currentTimestampWad <= termEndTimestampWad, "CT<ET");
+
+        int256 betaWad = marginCalculatorParameters.betaWad;
+
+        require(betaWad != 0, "B0");
+
+        int256 tMaxWad = marginCalculatorParameters.tMaxWad;
+
+        int256 scaledTimeWad = (int256(termEndTimestampWad) -
+            int256(currentTimestampWad)).div(tMaxWad);
+
+        int256 expInputWad = scaledTimeWad.mul(-betaWad);
+
+        timeFactorWad = expInputWad.exp();
+    }
+
+    /// @notice Calculates an APY Upper or Lower Bound of a given underlying pool (e.g. Aave v2 USDC Lending Pool)
+    /// @param termEndTimestampWad termEndTimestampScaled
+    /// @param currentTimestampWad currentTimestampScaled
+    /// @param historicalApyWad Geometric Mean Time Weighted Average APY (TWAPPY) of the underlying pool (e.g. Aave v2 USDC Lending Pool)
+    /// @param isUpper isUpper = true ==> calculating the APY Upper Bound, otherwise APY Lower Bound
+    /// @return apyBoundWad APY Upper or Lower Bound of a given underlying pool (e.g. Aave v2 USDC Lending Pool)
+    function computeApyBound(
+        uint256 termEndTimestampWad,
+        uint256 currentTimestampWad,
+        uint256 historicalApyWad,
+        bool isUpper
+    ) internal view returns (uint256 apyBoundWad) {
+        ApyBoundVars memory apyBoundVars;
+
+        int256 beta4Wad = marginCalculatorParameters.betaWad << 2;
+        int256 alpha4Wad = marginCalculatorParameters.alphaWad << 2;
+
+        apyBoundVars.timeFactorWad = computeTimeFactor(
+            termEndTimestampWad,
+            currentTimestampWad
+        );
+
+        apyBoundVars.oneMinusTimeFactorWad = ONE - apyBoundVars.timeFactorWad; // ONE is in wei
+
+        apyBoundVars.kWad = alpha4Wad.div(
+            marginCalculatorParameters.sigmaSquaredWad
+        );
+        apyBoundVars.zetaWad = (
+            marginCalculatorParameters.sigmaSquaredWad.mul(
+                apyBoundVars.oneMinusTimeFactorWad
+            )
+        ).div(beta4Wad);
+        apyBoundVars.lambdaNumWad = beta4Wad
+            .mul(apyBoundVars.timeFactorWad)
+            .mul(int256(historicalApyWad));
+        apyBoundVars.lambdaDenWad = marginCalculatorParameters
+            .sigmaSquaredWad
+            .mul(apyBoundVars.oneMinusTimeFactorWad);
+        apyBoundVars.lambdaWad = apyBoundVars.lambdaNumWad.div(
+            apyBoundVars.lambdaDenWad
+        );
+
+        apyBoundVars.criticalValueMultiplierWad =
+            ((apyBoundVars.lambdaWad << 1) + apyBoundVars.kWad) <<
+            1;
+
+        apyBoundVars.criticalValueWad = apyBoundVars
+            .criticalValueMultiplierWad
+            .sqrt()
+            .mul(
+                (isUpper)
+                    ? marginCalculatorParameters.xiUpperWad
+                    : marginCalculatorParameters.xiLowerWad
+            );
+
+        int256 apyBoundIntWad = apyBoundVars.zetaWad.mul(
+            apyBoundVars.kWad +
+                apyBoundVars.lambdaWad +
+                (
+                    isUpper
+                        ? apyBoundVars.criticalValueWad
+                        : -apyBoundVars.criticalValueWad
+                )
+        );
+
+        apyBoundWad = apyBoundIntWad < 0 ? 0 : uint256(apyBoundIntWad);
+    }
+
+    /// @notice Calculates the Worst Case Variable Factor At Maturity
+    /// @param timeInSecondsFromStartToMaturityWad Duration of a given IRS AMM (18 decimals)
+    /// @param termEndTimestampWad termEndTimestampWad
+    /// @param currentTimestampWad currentTimestampWad
+    /// @param isFT isFT => we are dealing with a Fixed Taker (short) IRS position, otherwise it is a Variable Taker (long) IRS position
+    /// @param isLM isLM => we are computing a Liquidation Margin otherwise computing an Initial Margin
+    /// @param historicalApyWad Historical Average APY of the underlying pool (e.g. Aave v2 USDC Lending Pool)
+    /// @return variableFactorWad The Worst Case Variable Factor At Maturity = APY Bound * accrualFactor(timeInYearsFromStartUntilMaturity) where APY Bound = APY Upper Bound for Fixed Takers and APY Lower Bound for Variable Takers (18 decimals)
+    function worstCaseVariableFactorAtMaturity(
+        uint256 timeInSecondsFromStartToMaturityWad,
+        uint256 termEndTimestampWad,
+        uint256 currentTimestampWad,
+        bool isFT,
+        bool isLM,
+        uint256 historicalApyWad
+    ) internal view returns (uint256 variableFactorWad) {
+        uint256 timeInYearsFromStartUntilMaturityWad = FixedAndVariableMath
+            .accrualFact(timeInSecondsFromStartToMaturityWad);
+
+        variableFactorWad = computeApyBound(
+            termEndTimestampWad,
+            currentTimestampWad,
+            historicalApyWad,
+            isFT
+        ).mul(timeInYearsFromStartUntilMaturityWad);
+
+        if (!isLM) {
+            variableFactorWad = variableFactorWad.mul(
+                isFT
+                    ? marginCalculatorParameters.apyUpperMultiplierWad
+                    : marginCalculatorParameters.apyLowerMultiplierWad
+            );
+        }
+    }
+
+    /// @notice calculates the absolute fixed token delta unbalanced resulting from a simulated counterfactual unwind necessary to determine the minimum margin requirement of a trader
+    /// @dev simulation of a swap without the need to involve the swap function
+    /// @param variableTokenDeltaAbsolute absolute value of the variableTokenDelta for which the unwind is simulated
+    /// @param sqrtRatioCurrX96 sqrtRatio necessary to calculate the starting fixed rate which is used to calculate the counterfactual unwind fixed rate
+    /// @param startingFixedRateMultiplierWad the multiplier (lambda from the litepaper - minimum margin requirement equation) that is multiplied by the starting fixed rate to determine the deviation applied to the starting fixed rate (in Wad)
+    /// @param fixedRateDeviationMinWad The minimum value the variable D (from the litepaper) can take
+    /// @param termEndTimestampWad term end timestamp in wad
+    /// @param currentTimestampWad current timestamp in wad
+    /// @param tMaxWad the maximum duration for a Voltz Protocol IRS AMM
+    /// @param gammaWad adjustable parameter that controls the rate of time decay applied to the deviation depending on time from now to maturity
+    /// @param isFTUnwind isFTUnwind == true => the counterfactual unwind is in the Fixed Taker direction (from left to right along the VAMM), the opposite is true if isFTUnwind == false
+    function getAbsoluteFixedTokenDeltaUnbalancedSimulatedUnwind(
+        uint256 variableTokenDeltaAbsolute,
+        uint160 sqrtRatioCurrX96,
+        uint256 startingFixedRateMultiplierWad,
+        uint256 fixedRateDeviationMinWad,
+        uint256 termEndTimestampWad,
+        uint256 currentTimestampWad,
+        uint256 tMaxWad,
+        uint256 gammaWad,
+        bool isFTUnwind
+    ) internal pure returns (uint256 fixedTokenDeltaUnbalanced) {
+        SimulatedUnwindLocalVars memory simulatedUnwindLocalVars;
+
+        /// @dev fixedRateDeviationMinWad is in percentage points
+
+        // calculate fixedRateStart
+
+        simulatedUnwindLocalVars.sqrtRatioCurrWad = FullMath.mulDiv(
+            ONE_UINT,
+            sqrtRatioCurrX96,
+            FixedPoint96.Q96
+        );
+
+        /// @dev fixedRateStartWad is in percentage points
+
+        simulatedUnwindLocalVars.fixedRateStartWad = ONE_UINT.div(
+            simulatedUnwindLocalVars.sqrtRatioCurrWad.mul(
+                simulatedUnwindLocalVars.sqrtRatioCurrWad
+            )
+        );
+
+        // calculate D (from the litepaper)
+        simulatedUnwindLocalVars.upperDWad = simulatedUnwindLocalVars
+            .fixedRateStartWad
+            .mul(startingFixedRateMultiplierWad);
+
+        if (simulatedUnwindLocalVars.upperDWad < fixedRateDeviationMinWad) {
+            simulatedUnwindLocalVars.upperDWad = fixedRateDeviationMinWad;
+        }
+
+        // calculate d (from the litepaper)
+
+        simulatedUnwindLocalVars.scaledTimeWad = (termEndTimestampWad -
+            currentTimestampWad).div(tMaxWad);
+
+        simulatedUnwindLocalVars.expInputWad = simulatedUnwindLocalVars
+            .scaledTimeWad
+            .toInt256()
+            .mul(-gammaWad.toInt256());
+        simulatedUnwindLocalVars.oneMinusTimeFactorWad =
+            ONE -
+            simulatedUnwindLocalVars.expInputWad.exp();
+
+        /// @audit-casting simulatedUnwindLocalVars.oneMinusTimeFactorWad is expected to be positive here, but what if goes below 0 due to rounding imprecision?
+        simulatedUnwindLocalVars.dWad = simulatedUnwindLocalVars.upperDWad.mul(
+            simulatedUnwindLocalVars.oneMinusTimeFactorWad.toUint256()
+        );
+
+        // calculate counterfactual fixed rate
+
+        if (isFTUnwind) {
+            if (
+                simulatedUnwindLocalVars.fixedRateStartWad >
+                simulatedUnwindLocalVars.dWad
+            ) {
+                simulatedUnwindLocalVars.fixedRateCFWad =
+                    simulatedUnwindLocalVars.fixedRateStartWad -
+                    simulatedUnwindLocalVars.dWad;
+            } else {
+                simulatedUnwindLocalVars.fixedRateCFWad = 0;
+            }
+        } else {
+            simulatedUnwindLocalVars.fixedRateCFWad =
+                simulatedUnwindLocalVars.fixedRateStartWad +
+                simulatedUnwindLocalVars.dWad;
+        }
+
+        // calculate fixedTokenDeltaUnbalancedWad
+
+        /// @dev simulatedUnwindLocalVars.fixedRateCFWad is in percentage points
+
+        simulatedUnwindLocalVars
+            .fixedTokenDeltaUnbalancedWad = variableTokenDeltaAbsolute
+            .fromUint()
+            .mul(simulatedUnwindLocalVars.fixedRateCFWad);
+
+        // calculate fixedTokenDeltaUnbalanced
+
+        fixedTokenDeltaUnbalanced = simulatedUnwindLocalVars
+            .fixedTokenDeltaUnbalancedWad
+            .toUint();
+    }
 
     // https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -1207,17 +1444,14 @@ contract MarginEngine is
         if (_variableTokenBalance != 0) {
             _exp2Wad = PRBMathSD59x18.mul(
                 _variableTokenBalanceWad,
-                MarginCalculator
-                    .worstCaseVariableFactorAtMaturity(
-                        _timeInSecondsFromStartToMaturityWad,
-                        _termEndTimestampWad,
-                        Time.blockTimestampScaled(),
-                        _variableTokenBalance < 0,
-                        _isLM,
-                        getHistoricalApy(),
-                        marginCalculatorParameters
-                    )
-                    .toInt256()
+                worstCaseVariableFactorAtMaturity(
+                    _timeInSecondsFromStartToMaturityWad,
+                    _termEndTimestampWad,
+                    Time.blockTimestampScaled(),
+                    _variableTokenBalance < 0,
+                    _isLM,
+                    getHistoricalApy()
+                ).toInt256()
             );
         }
 
@@ -1291,19 +1525,17 @@ contract MarginEngine is
 
         // simulate an adversarial unwind (cumulative position is a Variable Taker --> simulate FT unwind --> movement to the left along the VAMM)
         // fixedTokenDelta unbalanced that results from the simulated unwind
-        _fixedTokenDeltaUnbalanced = MarginCalculator
-            .getAbsoluteFixedTokenDeltaUnbalancedSimulatedUnwind(
-                uint256(_absoluteVariableTokenBalance),
-                _sqrtPriceX96,
-                _devMulWad,
-                _fixedRateDeviationMinWad,
-                _termEndTimestampWad,
-                Time.blockTimestampScaled(),
-                uint256(marginCalculatorParameters.tMaxWad),
-                marginCalculatorParameters.gammaWad,
-                _isVariableTokenBalancePositive
-            )
-            .toInt256();
+        _fixedTokenDeltaUnbalanced = getAbsoluteFixedTokenDeltaUnbalancedSimulatedUnwind(
+            uint256(_absoluteVariableTokenBalance),
+            _sqrtPriceX96,
+            _devMulWad,
+            _fixedRateDeviationMinWad,
+            _termEndTimestampWad,
+            Time.blockTimestampScaled(),
+            uint256(marginCalculatorParameters.tMaxWad),
+            marginCalculatorParameters.gammaWad,
+            _isVariableTokenBalancePositive
+        ).toInt256();
 
         int256 _fixedTokenDelta = FixedAndVariableMath.getFixedTokenBalance(
             _isVariableTokenBalancePositive
