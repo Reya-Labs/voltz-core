2ea66f7aa756b0d440bf8df846b8476b0f8eb6d5
tudor47 <tudorcostin7@gmail.com>
2022-05-26 12:03:06 +0300

    feat: got rid of ACL oz contract

--- contracts/VoltzPausable.sol
+++ contracts/VoltzPausable.sol
@@ -3,36 +3,30 @@
 pragma solidity =0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "hardhat/console.sol";
 
-contract VoltzPausable is PausableUpgradeable, AccessControlUpgradeable, OwnableUpgradeable {
-
+contract VoltzPausable is PausableUpgradeable, OwnableUpgradeable {
     bytes32 public constant VOLTZ_PAUSER = keccak256("VOLTZ_PAUSER");
 
+    mapping(address => bool) private pauser;
+
     function grantPauser(address account) external onlyOwner {
-        _grantRole(VOLTZ_PAUSER, account);
+        pauser[account] = true;
     }
 
     function revokePauser(address account) external onlyOwner {
-        _revokeRole(VOLTZ_PAUSER, account);
+        pauser[account] = false;
     }
 
     function pause() external {
-        _checkRole(VOLTZ_PAUSER, msg.sender);
+        require(pauser[msg.sender], "no pauser role");
         _pause();
     }
 
     function unpause() external {
-        _checkRole(VOLTZ_PAUSER, msg.sender);
+        require(pauser[msg.sender], "no pauser role");
         _unpause();
     }
-
-    // TODO: function to let those with VOLTZ_PAUSER role pause the contract - DONE
-    // TODO: functions to let the Owner grant & revoke Pauser rights - DONE
-    // TODO: Make all pausable contract inherit this - DONE
-    // TODO: reduce contract sizes (e.g. MarginEngine) back to acceptable levels
-    // TODO: write tests!
 }
