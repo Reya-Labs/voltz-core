3c1cf6d1f3c170dc2ca2aa30f3bdf01d5213b9e3
Artur Begyan <arturbegyan98@gmail.com>
2022-05-29 20:44:50 +0100

    linting

--- deploy/0.factory.ts
+++ deploy/0.factory.ts
@@ -1,7 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
-
 // todo: need a more elegant way to handle factory deployed via the community manager
 const DEPLOY_FACTORY: boolean = false;
 
