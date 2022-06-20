85021dea52269d704ab450fc76175a001aad8431
tudor47 <tudorcostin7@gmail.com>
2022-06-20 10:41:09 +0300

    fix: printer disabled in Tick.sol

--- contracts/core_libraries/Tick.sol
+++ contracts/core_libraries/Tick.sol
@@ -4,7 +4,6 @@ pragma solidity =0.8.9;
 import "../utils/LiquidityMath.sol";
 import "../utils/TickMath.sol";
 import "../utils/SafeCastUni.sol";
-import "../utils/Printer.sol";
 
 /// @title Tick
 /// @notice Contains functions for managing tick processes and relevant calculations
