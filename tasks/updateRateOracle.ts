42708ede008bb8b567e50079e01ee78cc06c4265
Soleiman Anwary <soleiman.anwary@gmail.com>
2022-08-30 13:03:48 +0100

    fix: change logic to optimise script

--- tasks/updateRateOracle.ts
+++ tasks/updateRateOracle.ts
@@ -28,88 +28,105 @@ async function main(
     "function variableFactor(uint256 termStartTimestampInWeiSeconds, uint256 termEndTimestampInWeiSeconds) public returns(uint256 resultWad)",
   ];
 
-  const factoryABI = [
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: true,
-          internalType: "contract IERC20Minimal",
-          name: "underlyingToken",
-          type: "address",
-        },
-        {
-          indexed: true,
-          internalType: "contract IRateOracle",
-          name: "rateOracle",
-          type: "address",
-        },
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "termStartTimestampWad",
-          type: "uint256",
-        },
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "termEndTimestampWad",
-          type: "uint256",
-        },
-        {
-          indexed: false,
-          internalType: "int24",
-          name: "tickSpacing",
-          type: "int24",
-        },
-        {
-          indexed: false,
-          internalType: "contract IMarginEngine",
-          name: "marginEngine",
-          type: "address",
-        },
-        {
-          indexed: false,
-          internalType: "contract IVAMM",
-          name: "vamm",
-          type: "address",
-        },
-        {
-          indexed: false,
-          internalType: "contract IFCM",
-          name: "fcm",
-          type: "address",
-        },
-        {
-          indexed: false,
-          internalType: "uint8",
-          name: "yieldBearingProtocolID",
-          type: "uint8",
-        },
-        {
-          indexed: false,
-          internalType: "uint8",
-          name: "underlyingTokenDecimals",
-          type: "uint8",
-        },
-      ],
-      name: "IrsInstance",
-      type: "event",
-    },
+  // const factoryABI = [
+  //     {
+  //         anonymous: false,
+  //         inputs: [
+  //             {
+  //                 indexed: true,
+  //                 internalType: "contract IERC20Minimal",
+  //                 name: "underlyingToken",
+  //                 type: "address",
+  //             },
+  //             {
+  //                 indexed: true,
+  //                 internalType: "contract IRateOracle",
+  //                 name: "rateOracle",
+  //                 type: "address",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "uint256",
+  //                 name: "termStartTimestampWad",
+  //                 type: "uint256",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "uint256",
+  //                 name: "termEndTimestampWad",
+  //                 type: "uint256",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "int24",
+  //                 name: "tickSpacing",
+  //                 type: "int24",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "contract IMarginEngine",
+  //                 name: "marginEngine",
+  //                 type: "address",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "contract IVAMM",
+  //                 name: "vamm",
+  //                 type: "address",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "contract IFCM",
+  //                 name: "fcm",
+  //                 type: "address",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "uint8",
+  //                 name: "yieldBearingProtocolID",
+  //                 type: "uint8",
+  //             },
+  //             {
+  //                 indexed: false,
+  //                 internalType: "uint8",
+  //                 name: "underlyingTokenDecimals",
+  //                 type: "uint8",
+  //             },
+  //         ],
+  //         name: "IrsInstance",
+  //         type: "event",
+  //     },
+  // ];
+
+  const marginEngineAddresses = [
+    "0x654316a63e68f1c0823f0518570bc108de377831", // aDAI
+    "0x0bc09825ce9433b2cdf60891e1b50a300b069dd2", // aUSDC
+    "0xf2ccd85a3428d7a560802b2dd99130be62556d30", // cDAI
+    "0x21f9151d6e06f834751b614c2ff40fc28811b235", // stETH
+    "0xb1125ba5878cf3a843be686c6c2486306f03e301", // rETH
+    "0x33bA6A0B16750206195c777879Edd8706204154B", // aUSDC borrow
+    "0x111A75E91625142E85193b67B10E53Acf82838cD", // cUSDT borrow
+    "0x9b76B4d09229c339B049053F171BFB22cbE50092", // aave ETH borrow
   ];
 
-  const factoryContract = new ethers.Contract(
-    "0x6a7a5c3824508D03F0d2d24E0482Bea39E08CcAF",
-    factoryABI,
-    signer
-  );
+  // programmatic retrieval of margin engines
+  // const factoryContract = new ethers.Contract(
+  //     "0x6a7a5c3824508D03F0d2d24E0482Bea39E08CcAF",
+  //     factoryABI,
+  //     signer
+  // );
 
-  const logs = await factoryContract.queryFilter(
-    factoryContract.filters.IrsInstance()
-  );
-  const marginEngineAddresses = logs.map(
-    (l) => factoryContract.interface.parseLog(l).args[5]
-  );
+  // const logs = await factoryContract.queryFilter(
+  //     factoryContract.filters.IrsInstance()
+  // );
+  // const marginEngineAddresses = logs.map(
+  //     (l) => factoryContract.interface.parseLog(l).args[5]
+  // );
+  // // Remove misconfigured pool '0x3A9a693114562512Bb7fcCF6f851C2fD84e62655'
+  // const index = marginEngineAddresses.indexOf('0x3A9a693114562512Bb7fcCF6f851C2fD84e62655');
+  // if (index > -1) { // only splice array when item is found
+  //     marginEngineAddresses.splice(index, 1); // 2nd parameter means remove one item only
+  // }
   console.log(
     "Printing Margin Engine Addresses and how many ",
     marginEngineAddresses.length,
@@ -117,7 +134,7 @@ async function main(
   );
 
   // settable parameter for max. allowable time delta before a new write should happen (24 hours for now)
-  const timeDelta = 86400;
+  const timeDeltaSeconds = 86400;
 
   for (let i = 0; i < marginEngineAddresses.length; i++) {
     console.log(
@@ -133,26 +150,26 @@ async function main(
     console.log("printing rate oracle address: ", rateOracleAddress);
 
     // get start time of margin engine i
-    const termStartTimestamp =
-      await await marginEngineContract.termStartTimestampWad();
+    const termStartTimestampWad =
+      await marginEngineContract.termStartTimestampWad();
     console.log(
-      "start timestamp: ",
-      termStartTimestamp.div(BigInt(1e18)).toNumber()
+      "start timestamp (sec): ",
+      termStartTimestampWad.div(BigInt(1e18)).toNumber()
     );
 
     // get end time of margin engine i
-    const termEndTimestamp =
-      await await marginEngineContract.termEndTimestampWad();
+    const termEndTimestampWad =
+      await marginEngineContract.termEndTimestampWad();
     console.log(
-      "end timestamp: ",
-      termEndTimestamp.div(BigInt(1e18)).toNumber()
+      "end timestamp (sec): ",
+      termEndTimestampWad.div(BigInt(1e18)).toNumber()
     );
 
     // get current block timestamp i.e. time now
-    const currentBlockTimestamp = await (
+    const currentBlockTimestampSeconds = await (
       await provider.getBlock("latest")
     ).timestamp;
-    console.log("current block timestamp:", currentBlockTimestamp);
+    console.log("current block timestamp:", currentBlockTimestampSeconds);
 
     const rateOracleContract = new ethers.Contract(
       rateOracleAddress,
@@ -168,8 +185,8 @@ async function main(
     );
     console.log("oracle observation: ", observation);
     // get the latest observation timestamp
-    const latestOracleQueryTime = observation.blockTimestamp;
-    console.log("oracle last query time: ", latestOracleQueryTime);
+    const latestOracleQueryTimeSeconds = observation.blockTimestamp;
+    console.log("oracle last query time: ", latestOracleQueryTimeSeconds);
 
     // get the min seconds since last rate update for this rate oracle
     const minSecondsSinceLastUpdate =
@@ -180,19 +197,17 @@ async function main(
     );
 
     const settlementRateCache = await rateOracleContract.settlementRateCache(
-      termStartTimestamp.div(BigInt(1e18)).toNumber(),
-      termEndTimestamp.div(BigInt(1e18)).toNumber()
+      termStartTimestampWad.div(BigInt(1e18)).toNumber(),
+      termEndTimestampWad.div(BigInt(1e18)).toNumber()
     );
     console.log("settlement cache rate: ", settlementRateCache.toNumber());
 
     if (
-      latestOracleQueryTime < termStartTimestamp &&
-      currentBlockTimestamp - latestOracleQueryTime > minSecondsSinceLastUpdate
+      latestOracleQueryTimeSeconds < termStartTimestampWad.div(BigInt(1e18)) &&
+      currentBlockTimestampSeconds > termStartTimestampWad.div(BigInt(1e18)) &&
+      currentBlockTimestampSeconds - latestOracleQueryTimeSeconds >
+        timeDeltaSeconds
     ) {
-      const writeEntry = await rateOracleContract.writeOracleEntry();
-      console.log("Successfully wrote to the oracle (start)");
-      await writeEntry.wait();
-    } else if (latestOracleQueryTime > timeDelta) {
       const writeEntry = await rateOracleContract.writeOracleEntry();
       console.log("Successfully wrote to the oracle (start)");
       await writeEntry.wait();
@@ -201,19 +216,12 @@ async function main(
     }
 
     if (
-      currentBlockTimestamp > termEndTimestamp &&
-      settlementRateCache.toNumber() === 0
+      currentBlockTimestampSeconds > termStartTimestampWad.div(BigInt(1e18)) &&
+      settlementRateCache.toString() === "0"
     ) {
       const vfTx = await rateOracleContract.variableFactor(
-        termStartTimestamp,
-        termEndTimestamp
-      );
-      await vfTx.wait();
-      console.log("Called Variable Factor");
-    } else if (currentBlockTimestamp - termEndTimestamp > timeDelta) {
-      const vfTx = await rateOracleContract.variableFactor(
-        termStartTimestamp,
-        termEndTimestamp
+        termStartTimestampWad,
+        termEndTimestampWad
       );
       await vfTx.wait();
       console.log("Called Variable Factor");
