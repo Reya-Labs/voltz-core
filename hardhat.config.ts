e6fb9a1c0526ec0df743c9251ba817ed3a991b40
Alexandru Niculae <niculae.alexandru1@gmail.com>
2022-11-07 16:46:30 +0000

    feat: updated get positions script and insolvency task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -122,7 +122,7 @@ if (!!process.env.FORK_MAINNET) {
     live: false,
     forking: {
       url: `${process.env.MAINNET_URL}`,
-      // blockNumber: 15402000,
+      // blockNumber: 15919000,
     },
   };
 } else if (!!process.env.FORK_KOVAN) {
--- tasks/checkInsolvencyAtMaturity.ts
+++ tasks/checkInsolvencyAtMaturity.ts
@@ -6,28 +6,24 @@ import {
 } from "../typechain";
 import { ethers, utils, BigNumber } from "ethers";
 
-import { getPositions, Position } from "../scripts/getPositions";
 import { SECONDS_PER_YEAR } from "@aave/protocol-js";
 import { toBn } from "../test/helpers/toBn";
 
+import * as poolAddresses from "../pool-addresses/mainnet.json";
+
 import Decimal from "decimal.js-light";
+import { getPositions, Position } from "../scripts/getPositions";
 
 task(
   "checkInsolvencyAtMaturity",
   "Checks the insolvency status of positions at maturity by estimating the expected cashflow."
 ).setAction(async (taskArgs, hre) => {
-  // deploy FixedAndVariableMath as contract to be able to call function
-  const fixedAndVariableMathFactory = await hre.ethers.getContractFactory(
-    "FixedAndVariableMathTest"
-  );
-  const fixedAndVariableMath =
-    (await fixedAndVariableMathFactory.deploy()) as FixedAndVariableMathTest;
+  const fixedAndVariableMath = (await hre.ethers.getContractAt(
+    "FixedAndVariableMathTest",
+    "0x2D2EE238Ca74B546BfA64864f5654b5Ed7673f87"
+  )) as FixedAndVariableMathTest;
 
-  const marginEngineAddresses = new Set<string>();
-  const positions: Position[] = await getPositions(true);
-  for (const position of positions) {
-    marginEngineAddresses.add(position.marginEngine);
-  }
+  const positions: Position[] = await getPositions();
 
   console.log("Positions estimated to become insolvent at maturity:");
   console.log(
@@ -41,7 +37,13 @@ task(
   );
 
   let noneInsolvent = true;
-  for (const marginEngineAddress of marginEngineAddresses) {
+  for (const poolName in poolAddresses) {
+    if (poolName === "default") {
+      continue;
+    }
+
+    const marginEngineAddress = poolAddresses[poolName as keyof typeof poolAddresses].marginEngine;
+
     const marginEngine = (await hre.ethers.getContractAt(
       "MarginEngine",
       marginEngineAddress
