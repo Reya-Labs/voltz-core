6077920029101a821f818c58d44b8e32dfcc6fd3
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-03-21 11:51:42 +0000

    feat: use getsigner from utils in estimate cashflow

--- tasks/estimateCashflow.ts
+++ tasks/estimateCashflow.ts
@@ -8,33 +8,9 @@ import {
 import { ethers, BigNumber } from "ethers";
 
 import { getPositions, Position } from "../scripts/getPositions";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { getSigner } from "./utils/getSigner";
 
 const SECONDS_PER_YEAR = 31536000;
-
-async function impersonateAccount(
-  hre: HardhatRuntimeEnvironment,
-  acctAddress: string
-) {
-  await hre.network.provider.request({
-    method: "hardhat_impersonateAccount",
-    params: [acctAddress],
-  });
-  // It might be a multisig contract, in which case we also need to pretend it has money for gas
-  await hre.ethers.provider.send("hardhat_setBalance", [
-    acctAddress,
-    "0x10000000000000000000",
-  ]);
-}
-
-async function getSigner(hre: HardhatRuntimeEnvironment, acctAddress: string) {
-  if (hre.network.name === "hardhat" || hre.network.name === "localhost") {
-    // We can impersonate the account
-    await impersonateAccount(hre, acctAddress);
-    return await hre.ethers.getSigner(acctAddress);
-  }
-  return null;
-}
 
 task(
   "estimateCashflow",
